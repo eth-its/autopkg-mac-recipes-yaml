19d1a3cd18e1ad122af45c891ffeae4112d83f7a
Graham Pugh <g.r.pugh@gmail.com>
2021-04-30 18:00:59 +0200

    EA must say None

--- Scripts/NiceUpdater2-EA.sh
+++ Scripts/NiceUpdater2-EA.sh
@@ -3,7 +3,7 @@
 nice_updater_version=$(pkgutil --pkg-info com.github.grahampugh.nice_updater | grep version | sed 's|version: ||' | cut -d. -f -3)
 
 if [[ "${nice_updater_version}" == *"No receipt"* ]]; then 
-    nice_updater_version=""
+    nice_updater_version="None"
 fi
 
 echo "<result>$nice_updater_version</result>"
