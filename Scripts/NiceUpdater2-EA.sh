84a2ef927f41c5c33b1b5f19264997f203b24ffc
Graham Pugh <g.r.pugh@gmail.com>
2021-04-30 18:15:36 +0200

    fix EA

--- Scripts/NiceUpdater2-EA.sh
+++ Scripts/NiceUpdater2-EA.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 
-nice_updater_version=$(pkgutil --pkg-info com.github.grahampugh.nice_updater | grep version | sed 's|version: ||' | cut -d. -f -3)
-
-if [[ "${nice_updater_version}" == *"No receipt"* ]]; then 
+if pkgutil --pkgs=com.github.grahampugh.nice_updater; then
+    nice_updater_version=$(pkgutil --pkg-info com.github.grahampugh.nice_updater | grep version | sed 's|version: ||' | cut -d. -f -3)
+else
     nice_updater_version="None"
 fi
 
