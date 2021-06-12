51e95f5c0b93c8256fb58fac0a79ac53e7fabd26
Graham Pugh <gpugh@ethz.ch>
2021-06-12 15:37:07 +0200

    try as current user

--- Scripts/ScreenshotDisableDropShadow.sh
+++ Scripts/ScreenshotDisableDropShadow.sh
@@ -2,6 +2,12 @@
 
 # https://themacbeginner.com/disable-drop-shadow-mac-os-screenshots/
 
-/usr/bin/defaults write com.apple.screencapture disable-shadow -bool TRUE
-killall SystemUIServer
+current_user=$(scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }')
+if [[ $current_user != "root" && $current_user != "_"*  ]]; then
+    sudo -u $current_user -i /usr/bin/defaults write com.apple.screencapture disable-shadow -bool TRUE
+    sudo -u $current_user -i killall SystemUIServer
+    echo "Set com.apple.screencapture disable-shadow and restarted UI server"
+else
+    echo "Not running script as no user found"
+fi
 
