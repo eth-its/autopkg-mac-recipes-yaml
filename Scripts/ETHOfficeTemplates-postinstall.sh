836f9641b99c0802cb0cb5e4ea046d2e5e32b7fe
Graham R Pugh <graham.pugh@id.ethz.ch>
2021-07-01 15:56:34 +0200

    chmod for the templates

--- Scripts/ETHOfficeTemplates-postinstall.sh
+++ Scripts/ETHOfficeTemplates-postinstall.sh
@@ -2,10 +2,19 @@
 ## ETH_Templates_MSOffice postinstall script
 ## Supports Office 2016, 2019 and 365
 
+templates_folder="/Library/Application Support/Microsoft/Office365/User Content.localized/Templates.localized"
+
+## change permissions
+echo "Changing permissions on imported templates"
+/bin/chmod -R 644 "$templates_folder"/*.potx "$templates_folder"/*.pdf "$templates_folder"/*/*.dotx
+
+
 # delete the __MACOSX artifact from the zip files
-if [[ -d "/Library/Application Support/Microsoft/Office365/User Content.localized/Templates.localized/__MACOSX" ]]; then 
-    rm -rf "/Library/Application Support/Microsoft/Office365/User Content.localized/Templates.localized/__MACOSX" ||:
+if [[ -d "$templates_folder/__MACOSX" ]]; then 
+    echo "Removing __MACOSX folder"
+    rm -rf "$templates_folder/__MACOSX" ||:
 fi
 
 ## write the version to a defaults file
+echo "Writing version to ch.ethz.id.ETHTemplatesMSOffice preferences"
 /usr/bin/defaults write /Library/Preferences/ch.ethz.id.ETHTemplatesMSOffice version -string "%version%"
