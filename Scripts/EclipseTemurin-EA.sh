2172a228cb416e78526cd686429099c66c4017b4
Graham Pugh <gpugh@ethz.ch>
2021-08-05 16:08:54 +0200

    Eclipse Temurin recipes

---
+++ Scripts/EclipseTemurin-EA.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+filter=%EXTENSION_ATTRIBUTE_VALUE%
+
+temp_file="/Users/Shared/jamf_java_versions_extension_attribute_check.plist"
+rm -f "$temp_file" ||:
+
+
+if [[ $filter == "8" ]]; then 
+    filter_name="1.8"
+elif [[ $filter == "11" ]]; then
+    filter_name="11"
+else
+    latest_jvm="None"
+fi
+
+if [[ $filter_name ]]; then
+    # get the JVM version using java_home
+    latest_jvm=$(/usr/libexec/java_home -X -v $filter_name | grep -A2 -m3 Temurin | tail -n 1 | sed 's|<[^>]*>||g' | sed 's|^[[:space:]]*||' | grep "$filter_name." )
+else
+    latest_jvm="None"
+fi
+
+[[ $latest_jvm == "" ]] && latest_jvm="None"
+
+echo "<result>$latest_jvm</result>"
+
+exit 0
---
+++ Scripts/EclipseTemurin-uninstall.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+filter="$4"
+
+if [[ ! $filter ]]; then
+    echo "No version supplied"
+    exit 1
+fi
+
+if [[ $filter -lt 9 ]]; then 
+    filter_name="1.$filter"
+else
+    filter_name="$filter"
+fi
+
+has_jvm=1
+while [[ $has_jvm == 1 ]]; do
+    echo "Checking for JVM..."
+    # get the JVM version using java_home
+    latest_jvm=$(/usr/libexec/java_home -X -v "$filter_name" | grep -A2 -m3 Temurin | tail -n 1 | sed 's|<[^>]*>||g' | sed 's|^[[:space:]]*||' | grep "$filter_name." )
+    if [[ $latest_jvm ]]; then
+        jvm_path=$( /usr/libexec/java_home -X -v "$filter_name" | grep -B9 -m10 Temurin | grep -A1 JVMHomePath | tail -n 1 | sed 's|<[^>]*>||g' | sed 's|^[[:space:]]*||' | sed 's|\/Contents\/Home||' )
+        if [[ -d "$jvm_path" ]]; then
+            echo "Deleting '$jvm_path'"
+            rm -Rf "$jvm_path"
+        else
+            echo "ERROR: $jvm_path is not a directory."
+        fi
+    else
+        echo "No JVM with version $filter is installed"
+        has_jvm=0
+    fi
+done
