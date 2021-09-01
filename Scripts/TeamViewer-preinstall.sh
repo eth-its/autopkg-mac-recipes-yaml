8466d995c54a46a6768414ce981474ef3786cca7
Graham Pugh <gpugh@ethz.ch>
2021-09-01 13:29:59 +0200

    change script to prevetn TeamViewer opening

--- Scripts/TeamViewer-preinstall.sh
+++ Scripts/TeamViewer-preinstall.sh
@@ -2,10 +2,8 @@
 ## preinstall script
 
 # Set TeamViewer to only restart the service on installation
-# This is achieved by creating the following file
-# before installing the package. the file should contain the
-# path to which TeamViewer should be installed, as this is
-# used elsewhere in the TeamViewer pkg postinstall script.
-# (thanks to @cdietrich)
+# This is achieved by creating the following files
+# before installing the package. 
 
-echo "/Applications" > /tmp/tvPath
+touch /tmp/silentInstaller
+touch /tmp/tvonlystartservice
