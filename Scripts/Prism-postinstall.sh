db9a20d27829763bf2969d3f516171c8b10035ca
Graham Pugh <g.r.pugh@gmail.com>
2021-11-03 11:22:56 +0100

    add email-collection line to Prism-postinstall.sh

--- Scripts/Prism-postinstall.sh
+++ Scripts/Prism-postinstall.sh
@@ -7,7 +7,7 @@ serial_number='%license_key%'
 config_path="/Library/Application Support/GraphPad/Prism"
 config_file="$config_path/prism-config.xml"
 
-config_contents="<configuration><silent-activation>true</silent-activation><serial-number>$serial_number</serial-number><check-for-updates>false</check-for-updates><measure-units>metric</measure-units></configuration>"
+config_contents="<configuration><silent-activation>true</silent-activation><serial-number>$serial_number</serial-number><check-for-updates>false</check-for-updates><measure-units>metric</measure-units><email-collection>disable</email-collection></configuration>"
 
 /bin/mkdir -p "$config_path"
 echo "$config_contents" > "$config_file"
