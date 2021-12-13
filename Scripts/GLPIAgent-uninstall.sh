ee276b6b4a650b3e70b43d16d46ebb04ce8ef9ac
Graham Pugh <g.r.pugh@gmail.com>
2021-12-13 09:33:16 +0100

    update for folder name change (no longer .app)

--- Scripts/GLPIAgent-uninstall.sh
+++ Scripts/GLPIAgent-uninstall.sh
@@ -9,10 +9,12 @@
 # Remove other components
 echo "Running uninstaller script"
 
-if ! /Applications/GLPI-Agent.app/uninstaller.sh ; then
-    echo "GLPI-Agent uninstaller script failed"
-else
+if /Applications/GLPI-Agent/uninstaller.sh ; then
     echo "GLPI-Agent uninstaller script succeeded"
+elif /Applications/GLPI-Agent.app/uninstaller.sh ; then
+    echo "GLPI-Agent.app uninstaller script succeeded"
+else
+    echo "GLPI-Agent uninstaller script failed"
 fi
 
 # Forget packages
