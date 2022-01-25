6cd64c44c6dfb4e64a4c457a1df0c04390130884
Graham Pugh <g.r.pugh@gmail.com>
2022-01-25 13:01:16 +0100

    Replace EXTENSION_ATTRIBUTE_SEARCH_VALUE key

--- Scripts/MicrosoftAutoUpdate-EA.sh
+++ Scripts/MicrosoftAutoUpdate-EA.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-MAUVersion=""
+MAUVersion="None"
 if [ -f "/Library/Application Support/Microsoft/MAU2.0/Microsoft AutoUpdate.app/Contents/Info.plist" ]; then
     MAUVersion=$(/usr/bin/defaults read "/Library/Application Support/Microsoft/MAU2.0/Microsoft AutoUpdate.app/Contents/Info.plist" CFBundleVersion)
 fi
