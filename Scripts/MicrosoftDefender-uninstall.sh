91c92a1c9c61ff0d2ae37f16e9be0b5e057278bc
Graham Pugh <g.r.pugh@gmail.com>
2022-01-31 11:13:09 +0100

    rename Defender ATP to Defender

--- Scripts/MicrosoftDefender-uninstall.sh
+++ Scripts/MicrosoftDefender-uninstall.sh
@@ -2,7 +2,7 @@
 
 #######################################################################
 #
-# MicrosoftDefenderATP-uninstall.sh Script for Jamf Pro
+# MicrosoftDefender-uninstall.sh Script for Jamf Pro
 #
 #######################################################################
 
@@ -12,10 +12,10 @@ uninstaller="/Library/Application Support/Microsoft/Defender/uninstall/uninstall
 if [[ -f "$uninstaller" ]]; then
     # try running the uninstaller
     if "$uninstaller"; then
-    echo "Microsoft Defender ATP uninstallation complete"
+    echo "Microsoft Defender uninstallation complete"
     else
-        echo "Something went wrong during the uninstallation of Microsoft Defender ATP"
+        echo "Something went wrong during the uninstallation of Microsoft Defender"
     fi
 else
-    echo "Microsoft Defender ATP uninstaller not found. Cannot proceed. Try Open Finder > Applications. Right click on Microsoft Defender for Endpoint > Move to Trash."
+    echo "Microsoft Defender uninstaller not found. Cannot proceed. Try Open Finder > Applications. Right click on Microsoft Defender for Endpoint > Move to Trash."
 fi
