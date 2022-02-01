938c5a32272f33ac7fcdc8adcded781526c5d8f1
Graham Pugh (iMac Pro) <graham@grahamrpugh.com>
2022-02-01 18:53:42 +0100

    Privileges

---
+++ Scripts/Privileges-uninstall.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+#######################################################################
+#
+# Application Uninstaller Script for Jamf Pro
+#
+# This script can delete apps that are sandboxed and live in /Applications
+#
+# Adapted for Privileges app
+#
+#######################################################################
+
+function silent_app_quit() {
+    # silently kill the application.
+    # add .app to end of string if not supplied
+    app_name="${app_name/\.app/}"            # remove any .app
+    check_app_name="${app_name/\(/\\(}"       # escape any brackets for the pgrep
+    check_app_name="${check_app_name/\)/\\)}"  # escape any brackets for the pgrep
+    check_app_name="${check_app_name}.app"     # add the .app back
+    if pgrep -f "/${check_app_name}" ; then
+        echo "Closing $check_app_name"
+        /usr/bin/osascript -e "quit app \"$app_name\"" &
+        sleep 1
+
+        # double-check
+        n=0
+        while [[ $n -lt 10 ]]; do
+            if pgrep -f "/$check_app_name" ; then
+                (( n=n+1 ))
+                sleep 1
+				echo "Graceful close attempt # $n"
+            else
+                echo "$app_name closed."
+                break
+            fi
+        done
+        if pgrep -f "/$check_app_name" ; then
+            echo "$check_app_name failed to quit - killing."
+            /usr/bin/pkill -f "/$check_app_name"
+        fi
+    fi
+}
+
+# Inputted variables
+app_name="Privileges"
+
+# quit the app if running
+silent_app_quit "$app_name"
+
+# Now remove the app
+echo "Removing application: ${app_name}"
+
+# Add standard path if none provided
+if [[ ! $app_name == *"/"* ]]; then
+	app_to_trash="/Applications/$app_name.app"
+else
+	app_to_trash="$app_name.app"
+fi
+
+echo "Application will be deleted: $app_to_trash"
+# Remove the application
+/bin/rm -Rf "${app_to_trash}"
+
+echo "Checking if $app_name is actually deleted..."
+if [[ -d "${app_to_trash}" ]]; then
+    echo "$app_name failed to delete"
+else
+    echo "$app_name deleted successfully"
+fi
+
+# also check to see if an additional app was ever created due to BundleID mismatch
+if [[ -d "/Applications/${app_name}/${app_name}.app" ]]; then
+    echo "Folder will be deleted: /Applications/${app_name}/"
+    /bin/rm -Rf "/Applications/${app_name}" ||:
+else
+    echo "Folder not found: /Applications/${app_name}/"
+fi
+if [[ -d "/Applications/${app_name}.localized/${app_name}.app" ]]; then
+    echo "Folder will be deleted: /Applications/${app_name}.localized/"
+    /bin/rm -Rf "/Applications/${app_name}.localized" ||:
+else
+    echo "Folder not found: /Applications/${app_name}.localized/"
+fi
+
+# Get current user
+current_user=$( echo "show State:/Users/ConsoleUser" | scutil | awk '/Name :/ { print $3 }' )
+
+# Stop the current launchagent and remove it prior to installing this updated one
+echo "Removing LaunchAgent"
+
+uid=$(id -u "$current_user")
+/bin/launchctl asuser "$uid" /bin/launchctl unload -w "/Library/LaunchAgents/corp.sap.privileges.plist" ||:
+/bin/rm -f "/Library/LaunchAgents/corp.sap.privileges.plist" ||:
+
+# Wait for 2 seconds
+Sleep 2
+
+# Additional files to delete
+echo "Deleting other files"
+demote_script_location="/Library/Management/ETHZ/Privileges"
+/bin/rm -Rf "$demote_script_location" ||:
+
+# Forget packages (works for all versions)
+echo "Forgetting packages"
+/usr/sbin/pkgutil --forget com.sap.privileges ||:
