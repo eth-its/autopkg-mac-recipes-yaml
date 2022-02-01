408cad09d0e55c9ec4c690e41977106ebf29f0da
Graham Pugh (iMac Pro) <graham@grahamrpugh.com>
2022-02-01 19:28:14 +0100

    add dock item

--- Scripts/Privileges-postinstall.sh
+++ Scripts/Privileges-postinstall.sh
@@ -84,9 +84,3 @@ sudo /bin/chmod 644 "/Library/LaunchAgents/corp.sap.privileges.plist"
 
 uid=$(id -u "$current_user")
 /bin/launchctl asuser "$uid" /bin/launchctl load -w "/Library/LaunchAgents/corp.sap.privileges.plist"
-
-# add item to the dock
-sudo -u "$current_user" -i /usr/bin/defaults write com.apple.dock persistent-apps -array-add '<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/Privileges.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>'
-
-/usr/bin/killall cfprefsd 
-/usr/bin/killall Dock
