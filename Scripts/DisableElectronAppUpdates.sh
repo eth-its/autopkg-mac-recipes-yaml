e2ac7af866c703ddaeddf6616c635c7a35007c6a
Graham Pugh (iMac Pro) <graham@grahamrpugh.com>
2022-03-09 12:06:08 +0100

    fix the script for launchagent

--- Scripts/DisableElectronAppUpdates.sh
+++ Scripts/DisableElectronAppUpdates.sh
@@ -9,14 +9,32 @@ such as Skype and Atom
 See https://soundmacguy.wordpress.com/2018/02/25/microsoft-skype-definitely-and-teams-maybe-disabling-automatic-updates/
 DOC
 
+# current logged-in user's short name
+current_user=$(/bin/ls -la /dev/console | /usr/bin/cut -d " " -f 4)
+uid=$(id -u "$current_user")
+echo "Current user is $current_user"
 
 # Create the launchagent file in the library and write to it
 launchagent="/Library/LaunchAgents/com.github.eth-its.disable-electron-update-notifications.plist"
 
 # reset any old version
 if [[ -f "$launchagent" ]]; then
-    /bin/launchctl bootout "$launchagent" ||:
-    /bin/rm "$launchagent"
+    if /bin/launchctl disable "user/$uid/com.github.eth-its.disable-electron-update-notifications"; then
+        echo "LaunchAgent successfully disabled"
+    else
+        echo "LaunchAgent was not disabled."
+    fi
+    if sudo -u "$current_user" -i /bin/launchctl unload -F "$launchagent"; then
+        echo "LaunchAgent successfully unloaded"
+    else
+        echo "LaunchAgent was not unloaded."
+    fi
+
+    if /bin/rm "$launchagent"; then
+        echo "LaunchAgent successfully deleted"
+    else
+        echo "LaunchAgent was not deleted."
+    fi
 fi
 
 cat > "$launchagent" <<END 
@@ -46,5 +64,14 @@ sleep 2
 /usr/sbin/chown root:wheel "$launchagent"
 /bin/chmod 644 "$launchagent"
 
-/bin/launchctl enable system/com.github.eth-its.disable-electron-update-notifications
-/bin/launchctl bootstrap system "$launchagent"
+if sudo -u "$current_user" -i /bin/launchctl load -F "$launchagent"; then
+    echo "LaunchAgent successfully loaded"
+else
+    echo "LaunchAgent was not loaded."
+fi
+
+if sudo -u "$current_user" -i /bin/launchctl enable "user/$uid/com.github.eth-its.disable-electron-update-notifications"; then
+    echo "LaunchAgent successfully enabled"
+else
+    echo "LaunchAgent was not enabled."
+fi
