bf7b26506310ae77973776c85d068dd945453504
Graham Pugh (iMac Pro) <graham@grahamrpugh.com>
2022-04-28 09:48:28 +0200

    Switch Temurin for Eclipse

--- Scripts/EclipseTemurin-uninstall.sh
+++ Scripts/EclipseTemurin-uninstall.sh
@@ -17,9 +17,9 @@ has_jvm=1
 while [[ $has_jvm == 1 ]]; do
     echo "Checking for JVM..."
     # get the JVM version using java_home
-    latest_jvm=$(/usr/libexec/java_home -X -v "$filter_name" | grep -A2 -m3 Temurin | tail -n 1 | sed 's|<[^>]*>||g' | sed 's|^[[:space:]]*||' | grep "$filter_name." )
+    latest_jvm=$(/usr/libexec/java_home -X -v "$filter_name" | grep -A3 JVMVendor | grep -A2 Eclipse | tail -n 1 | sed 's|<[^>]*>||g' | sed 's|^[[:space:]]*||' | grep "$filter_name." )
     if [[ $latest_jvm ]]; then
-        jvm_path=$( /usr/libexec/java_home -X -v "$filter_name" | grep -B9 -m10 Temurin | grep -A1 JVMHomePath | tail -n 1 | sed 's|<[^>]*>||g' | sed 's|^[[:space:]]*||' | sed 's|\/Contents\/Home||' )
+        jvm_path=$( /usr/libexec/java_home -X -v "$filter_name" | grep -B9 -m10 Eclipse | grep -A1 JVMHomePath | tail -n 1 | sed 's|<[^>]*>||g' | sed 's|^[[:space:]]*||' | sed 's|\/Contents\/Home||' )
         if [[ -d "$jvm_path" ]]; then
             echo "Deleting '$jvm_path'"
             rm -Rf "$jvm_path"
