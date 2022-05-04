faed39143ff39c0ee3b8604e766074d3de7a0609
Graham Pugh (iMac Pro) <graham@grahamrpugh.com>
2022-05-04 11:02:46 +0200

    add logic to the XCLI installation

--- Scripts_for_Installers/Fiji-preinstall.zsh
+++ Scripts_for_Installers/Fiji-preinstall.zsh
@@ -8,4 +8,13 @@ if [ -d "$3/Applications/Fiji.app" ]; then
 fi
 
 # The internal updater requires the Xcode command line tools (for git)
-jamf policy -event XcodeCLTools-install
+if xcode-select -p >/dev/null 2>&1; then
+    result="Installed"
+else
+    result="Not installed"
+fi
+
+if [[ "$result" == "Not installed" ]]; then
+   echo "The Xcode Command Line Tools are required to use Fiji. Installing now."
+   jamf policy -event XcodeCLTools-install
+fi
