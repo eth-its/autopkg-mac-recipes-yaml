ef118865f0c2c6b458975026b43d3d0d2f1e26b9
Graham Pugh (iMac Pro) <graham@grahamrpugh.com>
2022-06-14 16:43:31 +0200

    IBM Spectrum Protect

---
+++ Scripts_for_Installers/IBMSpectrumProtect-preinstall.zsh
@@ -0,0 +1,25 @@
+#!/bin/zsh
+# shellcheck shell=bash
+
+# Remove existing installation(s) if it is in the Applications folder or a subfolder
+
+appName="IBM Spectrum Protect"
+
+# quit the app if running
+pkill dsmj
+pkill TSMToolsforAdministrators
+
+# look for any existing certificates and move them out of the way
+tempdir=/var/folders/tivoli-reinstall
+certdir="/Library/Application Support/tivoli/tsm/client/ba/bin"
+if [[ -d "$certdir" ]]; then
+    mkdir -p $tempdir
+    cp "$certdir/dsmcert.kdb" "$certdir/dsmcert.sth" "$certdir/dsmcert.idx" $tempdir
+fi
+
+# Now remove the app
+echo "Removing application: ${appName}"
+
+if [[ -d "/Applications/${appName}/${appName}.app" ]]; then
+    jamf policy -event "IBM Spectrum Protect-uninstall"
+fi
---
+++ Scripts_for_Uninstallers/IBMSpectrumProtect-uninstall.zsh
@@ -0,0 +1,51 @@
+#!/bin/zsh
+# shellcheck shell=bash
+
+# Uninstall IBM Spectrum Protect
+
+appName="IBM Spectrum Protect"
+
+function silent_app_quit() {
+    # silently kill the application.
+    appName="$1"
+    if [[ $(pgrep -ix "$appName") ]]; then
+    	echo "Closing $appName"
+    	/usr/bin/osascript -e "quit app \"$appName\""
+    	sleep 1
+
+    	# double-check
+    	countUp=0
+    	while [[ $countUp -le 10 ]]; do
+    		if [[ -z $(pgrep -ix "$appName") ]]; then
+    			echo "$appName closed."
+    			break
+    		else
+    			let countUp=$countUp+1
+    			sleep 1
+    		fi
+    	done
+        if [[ $(pgrep -x "$appName") ]]; then
+    	    echo "$appName failed to quit - killing."
+    	    /usr/bin/pkill "$appName"
+        fi
+    fi
+}
+# quit the app if running
+silent_app_quit "$appName"
+
+echo "Removing application: ${appName}"
+
+/bin/rm -rf "/Applications/IBM Spectrum Protect" ||:
+/bin/rm -rf "/Library/Application Support/tivoli" ||:
+/bin/rm -f "/usr/bin/dsmc" "/usr/bin/dsmcad" "/usr/bin/dsmadmc" "/usr/bin/dsmtrace" "/usr/bin/dsmagent" "/usr/lib/libxmlutil-6.2.0.dylib" "/usr/lib/libtsm620xerces-c1_6_0.dylib" ||:
+
+fm_receipts="com.ibm.tivoli.tsm.baclient
+com.ibm.tivoli.tsm.baclientjava"
+
+# Loop through the remaining parameters
+pkgutilcmd="/usr/sbin/pkgutil"
+while read -r receipt; do
+	$pkgutilcmd --pkgs=${receipt} && $pkgutilcmd --forget ${receipt}
+done <<< "${receipts}"
+
+echo "${appName} removal complete!"
