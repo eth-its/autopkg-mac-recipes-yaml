7231b452d36412a75e0c2c50804f3369798124ff
Graham Pugh (iMac Pro) <graham@grahamrpugh.com>
2022-06-21 21:05:13 +0200

    Add EA

---
+++ Scripts_for_EAs/Maple-EA.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Maple license detector
+
+management_dir="/Library/Management/ETHZ/Maple"
+node_license_store="$management_dir/.Node.license"
+floating_license_store="$management_dir/.Floating.license"
+
+active_license="/Library/Frameworks/Maple.framework/Versions/current/license"
+
+if [[ -f "$active_license" ]]; then
+	if cmp -s "$active_license" "$node_license_store"; then
+		license_type="Node"	
+	elif cmp -s "$active_license" "$floating_license_store"; then
+		license_type="Floating"
+	else
+		license_type="Invalid"
+	fi
+else
+	license_type="None"
+fi
+
+echo "<result>$license_type</result>"
