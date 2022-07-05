42cdf4c398c71460aacb5806fbda18ac22feeefc
Graham Pugh (iMac Pro) <graham@grahamrpugh.com>
2022-07-05 11:26:07 +0200

    MOE recipes

---
+++ Scripts_for_Installers/MOE-postinstall.zsh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+# shellcheck shell=bash
+
+: <<DOC
+Configures the MOE license.dat file
+
+Required parameters:
+4 - Major Version e.g. 2019.0102
+4 - Server string e.g. 123.145.167.89 001122334455 6677
+DOC
+
+echo "SERVER $5
+USE_SERVER" > "/Applications/moe_$4/license.dat"
