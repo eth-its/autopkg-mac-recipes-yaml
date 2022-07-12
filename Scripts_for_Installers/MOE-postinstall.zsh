346d111d8745678f9e833525fb1cd3d0723507e5
Graham Pugh <g.r.pugh@gmail.com>
2022-07-12 09:48:17 +0200

    more tweaks to moe

--- Scripts_for_Installers/MOE-postinstall.zsh
+++ Scripts_for_Installers/MOE-postinstall.zsh
@@ -9,5 +9,7 @@ Required parameters:
 4 - Server string e.g. 123.145.167.89 001122334455 6677
 DOC
 
+folder_name=$(find /Applications -name "moe*$MAJOR_VERSION" -type d -maxdepth 1 | head -n 1)
+
 echo "SERVER $5
-USE_SERVER" > "/Applications/moe$4/license.dat"
+USE_SERVER" > "$folder_name/license.dat"
