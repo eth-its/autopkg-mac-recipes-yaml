7d565237ea968cc54b5dc9d87c268ccd36946311
Graham Pugh <noemail@itscdgpmacmini>
2022-09-14 13:53:12 +0200

    Volume Owner

---
+++ Scripts_for_EAs/VolumeOwners-EA.zsh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+
+## List Volume Owners
+
+enabled_users=()
+
+while IFS= read line ; do
+    enabled_users+="$(echo $line | cut -d, -f1)"
+done <<< "$(/usr/bin/fdesetup list)"
+
+[[ -z $enabled_users ]] && enabled_users=("None")
+
+result="<result>"
+for user in "${enabled_users[@]}"; do
+    result+="${user},"
+done
+result=$(echo "$result" | sed 's|,$|</result>|')
+
+echo "$result"
+exit 0
