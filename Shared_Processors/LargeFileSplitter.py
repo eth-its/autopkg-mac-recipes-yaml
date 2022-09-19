82394702530671309dbe14016aabacc5718f3f5c
Graham Pugh <g.r.pugh@gmail.com>
2022-09-19 21:32:41 +0200

    try splitting the file

---
+++ Shared_Processors/LargeFileSplitter.py
@@ -0,0 +1,61 @@
+#!/usr/local/autopkg/python
+#
+# Copyright 2022 by Graham Pugh
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""See docstring for LargeFileSplitter class"""
+
+import os.path
+import shutil
+import subprocess
+
+from autopkglib import Processor
+
+
+__all__ = ["LargeFileSplitter"]
+
+
+class LargeFileSplitter(Processor):
+    """Splits a large file into 500 MB chunks."""
+
+    description = __doc__
+    input_variables = {
+        "large_file_path": {
+            "required": True,
+            "description": ("Path to a large file that should be split up"),
+        },
+        "parts_dir": {
+            "required": True,
+            "description": ("Directory to store the split file parts."),
+        },
+    }
+    output_variables = {}
+
+    def split(self, large_file_path, parts_dir):
+        """Do the split"""
+        large_file = os.path.basename(large_file_path)
+        shutil.copy(large_file_path, parts_dir)
+        split_cmd = f"/usr/bin/split -b 500m '{parts_dir}/{large_file}'"
+        # run the command
+        subprocess.check_output(split_cmd)
+
+    def main(self):
+        """Split that file!"""
+        large_file_path = self.env.get("large_file_path")
+        parts_dir = self.env.get("parts_dir")
+        self.split(large_file_path, parts_dir)
+
+
+if __name__ == "__main__":
+    PROCESSOR = LargeFileSplitter()
+    PROCESSOR.execute_shell()
