70c10cbcf9e698a86d7e10eb275b030168f62cdf
Graham Pugh <g.r.pugh@gmail.com>
2022-10-02 18:12:22 +0200

    switch to number of updates

--- Scripts_for_EAs/SoftwareUpdates-EA.zsh
+++ Scripts_for_EAs/SoftwareUpdates-EA.zsh
@@ -2,12 +2,7 @@
 # shellcheck shell=bash
 
 # Check for whether there are any Software Updates available
-
-if /usr/libexec/PlistBuddy -c "Print :RecommendedUpdates:0" /Library/Preferences/com.apple.SoftwareUpdate.plist 2>/dev/null; then
-    result="true"
-else
-    result="false"
-fi
+result=$(/usr/libexec/PlistBuddy -c "Print :LastRecommendedUpdatesAvailable" /Library/Preferences/com.apple.SoftwareUpdate.plist 2>/dev/null)
 
 echo "<result>$result</result>"
 exit 0
