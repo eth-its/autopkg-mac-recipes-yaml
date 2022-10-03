865a49bd1921a5ab16f0dd4342b907a775c44355
Graham Pugh (iMac Pro) <graham@grahamrpugh.com>
2022-10-03 20:14:10 +0200

    prevent skipping

--- Scripts_for_Installers/CiscoSecureClient-postinstall.zsh
+++ Scripts_for_Installers/CiscoSecureClient-postinstall.zsh
@@ -6,25 +6,29 @@
 # https://macadmins.slack.com/archives/CK38R7KPS/p1645729119584389?thread_ts=1645726163.719389&cid=CK38R7KPS
 
 user=$(/usr/bin/stat -f %Su "/dev/console")
-ACSOCKEXTACTIVE=$(/usr/bin/systemextensionsctl list | grep acsockext)
 
 # give the extension a chance to be installed
-sleep 10
+timeout=120
+i=1
+while ! /usr/bin/systemextensionsctl list | grep acsockext; do
+    sleep 1
+    i=((i+1))
+    if [[ i -ge $timeout ]]; then
+        echo "ERROR: timed out - Skip deactivating of network extension acsockext"
+        exit
+    fi
+do
 
-if [ ! "x${ACSOCKEXTACTIVE}" = "x" ]; then
-    echo "Deactivating network extension acsockext"
-    # Need to run as logged in user, as user needs to authorize the extension deactivation.
-    if /usr/bin/su $user -c '/Applications/Cisco/Cisco\ Secure\ Client\ -\ Socket\ Filter.app/Contents/MacOS/Cisco\ Secure\ Client\ -\ Socket\ Filter -deactivateExt' ; then
-        echo "Network extension acsockext deactivated"
-        # Remove the network extension KDF app.
-        if /bin/rm -Rf "/Applications/Cisco/Cisco Secure Client - Socket Filter.app" ; then
-            echo "Cisco Secure Client Socket Filter deleted"
-        else
-            echo "ERROR: Cisco Secure Client Socket Filter not deleted"
-        fi
+echo "Deactivating network extension acsockext"
+# Need to run as logged in user, as user needs to authorize the extension deactivation.
+if /usr/bin/su $user -c '/Applications/Cisco/Cisco\ Secure\ Client\ -\ Socket\ Filter.app/Contents/MacOS/Cisco\ Secure\ Client\ -\ Socket\ Filter -deactivateExt' ; then
+    echo "Network extension acsockext deactivated"
+    # Remove the network extension KDF app.
+    if /bin/rm -Rf "/Applications/Cisco/Cisco Secure Client - Socket Filter.app" ; then
+        echo "Cisco Secure Client Socket Filter deleted"
     else
-        echo "ERROR: Network extension acsockext not deactivated"
+        echo "ERROR: Cisco Secure Client Socket Filter not deleted"
     fi
 else
-    echo "Skip deactivating of network extension acsockext"
+    echo "ERROR: Network extension acsockext not deactivated"
 fi
