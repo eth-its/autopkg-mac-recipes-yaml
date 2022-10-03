b0578cabbc48084c5332c8d13491ce0c3091b1d9
Graham Pugh (iMac Pro) <graham@grahamrpugh.com>
2022-10-03 23:31:08 +0200

    better removal of launchagent

--- Scripts_for_Installers/CitrixWorkspace-postinstall.zsh
+++ Scripts_for_Installers/CitrixWorkspace-postinstall.zsh
@@ -1,8 +1,35 @@
 #!/bin/zsh
 # shellcheck shell=bash
 
-# remove Citrix Workspace LaunchAgent
-# originally from Owen Gallagher
-# https://discussions.citrix.com/topic/400690-macos-prevent-workspacereceiver-from-starting-at-loginboot/
+# disable and remove Citrix Workspace LaunchAgent
 
-rm /Library/LaunchAgents/com.citrix.ReceiverHelper.plist
+if pgrep "Citrix Workspace"; then
+    pkill "Citrix Workspace"
+fi
+
+# current logged-in user's short name
+current_user=$(/bin/ls -la /dev/console | /usr/bin/cut -d " " -f 4)
+uid=$(id -u "$current_user")
+echo "Current user is $current_user"
+
+launchagent="/Library/LaunchAgents/com.citrix.ReceiverHelper.plist"
+
+# reset any old version
+if [[ -f "$launchagent" ]]; then
+    if /bin/launchctl disable "user/$uid/com.citrix.ReceiverHelper"; then
+        echo "LaunchAgent successfully disabled"
+    else
+        echo "LaunchAgent was not disabled."
+    fi
+    if sudo -u "$current_user" -i /bin/launchctl unload -F "$launchagent"; then
+        echo "LaunchAgent successfully unloaded"
+    else
+        echo "LaunchAgent was not unloaded."
+    fi
+
+    if /bin/rm "$launchagent"; then
+        echo "LaunchAgent successfully deleted"
+    else
+        echo "LaunchAgent was not deleted."
+    fi
+fi
