3342518ebc7f490a77f1a952dad294bff48b354a
Graham Pugh (iMac Pro) <graham@grahamrpugh.com>
2022-10-07 21:53:07 +0200

    syntax error

--- Scripts_for_EAs/CSDS-EA.zsh
+++ Scripts_for_EAs/CSDS-EA.zsh
@@ -6,12 +6,11 @@ version="None"
 
 # look for the CSDS folder
 if [[ -d "/Applications/CSDS %MAJOR_VERSION%" ]]; then
-        # now look for the version_info file in the GOLD folder
-        version_file="/Applications/CSDS %MAJOR_VERSION%/Discovery_%MAJOR_VERSION%/GOLD/version_info"
-        if [[ -f "$version_file" ]]; then
-            # now read the version from the file
-            version=$(cat "$version_file" | cut -d" " -f3)
-        fi
+    # now look for the version_info file in the GOLD folder
+    version_file="/Applications/CSDS %MAJOR_VERSION%/Discovery_%MAJOR_VERSION%/GOLD/version_info"
+    if [[ -f "$version_file" ]]; then
+        # now read the version from the file
+        version=$(cat "$version_file" | cut -d" " -f3)
     fi
 fi
 
