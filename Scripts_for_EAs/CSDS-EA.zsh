3eee2f078ff097491a9fd358f25eaaf1894fcdfd
Graham Pugh (iMac Pro) <graham@grahamrpugh.com>
2022-10-07 20:08:32 +0200

    fix the EA for new folders

--- Scripts_for_EAs/CSDS-EA.zsh
+++ Scripts_for_EAs/CSDS-EA.zsh
@@ -7,10 +7,9 @@ version="None"
 # look for the CSDS folder
 if [[ -d "/Applications/CSDS" ]]; then
     # now look for the major version
-    if discovery_folder=$(find /Applications/CSDS -name "Discovery_*" -maxdepth 1 -type d 2> /dev/null); then
-        major_version=$(basename "$discovery_folder" | cut -d_ -f2)
+    if csds_folder=$(find /Applications -name "CSDS %MAJOR_VERSION%" -maxdepth 1 -type d 2> /dev/null); then
         # now look for the version_info file in the GOLD folder
-        version_file="/Applications/CSDS/Discovery_$major_version/GOLD/version_info"
+        version_file="/Applications/CSDS %MAJOR_VERSION%/Discovery_%MAJOR_VERSION%/GOLD/version_info"
         if [[ -f "$version_file" ]]; then
             # now read the version from the file
             version=$(cat "$version_file" | cut -d" " -f3)
