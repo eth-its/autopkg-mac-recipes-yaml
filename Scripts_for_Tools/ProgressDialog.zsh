e777a9999a014f6c9decd95e4ab963673f4a971b
Graham Pugh <g.r.pugh@gmail.com>
2022-10-17 02:15:13 +0200

    improved logic

--- Scripts_for_Tools/ProgressDialog.zsh
+++ Scripts_for_Tools/ProgressDialog.zsh
@@ -9,52 +9,60 @@ jamf_log="/var/log/jamf.log"
 dialog_log=$(mktemp /var/tmp/dialog.XXX)
 chmod 644 ${dialog_log}
 script_log="/var/tmp/jamfprogress.log"
+lock_file="/var/tmp/lock.txt"
+icon="/Applications/ETH Self Service.app"
 count=0
+
 # Location of this script
 progress_script_location="/Library/Management/ETHZ/Progress"
-lock_file="$progress_script_location/lock.txt"
 
 function update_log() {
-    echo "$(date) ${1}" >> $script_log
+    /bin/echo "$(date) ${1}" >> "$script_log"
 }
 
 function dialog_cmd() {
-    echo "${1}" >> "${dialog_log}"
+    /bin/echo "${1}" >> "${dialog_log}"
     sleep 0.1
 }
 
 function launch_dialog() {
-	update_log "launching main dialog"
-    open -a "/Library/Application Support/Dialog/Dialog.app" --args --mini --title "${policy_name}" --icon "${icon}" --message "Please wait for the installation to complete.  \nYou may need to restart the application afterwards." --progress 8 --commandfile "${dialog_log}"
+	update_log "launching main dialog with log ${dialog_log}"
+    /usr/local/bin/dialog --mini --title "${policy_name}" --icon "${icon}" --message "Please wait for the process to complete." --progress 8 --commandfile "${dialog_log}" &
+    PID=$!
     update_log "main dialog running in the background with PID $PID"
+    sleep 0.1
 }
 
 function dialog_error() {
 	update_log "launching error dialog"
     errormsg="### Error\n\nSomething went wrong. Please contact IT support and report the following error message:\n\n${1}"
-    open -a "/Library/Application Support/Dialog/Dialog.app" --args --ontop --title "Jamf Policy Error" --icon "${icon}" --overlayicon caution --message "${errormsg}"
+    /usr/local/bin/dialog --ontop --moveable --position bottomright --title "Jamf Policy Error" --icon "${icon}" --overlayicon caution --message "${errormsg}"
+    PID=$!
     update_log "error dialog running in the background with PID $PID"
+    sleep 0.1
 }
 
 function quit_script() {
-	update_log "quit_script was called"
+	update_log "sending quit command"
     dialog_cmd "quit: "
-    sleep 1
-    update_log "Exiting"
+    sleep 0.1
     # brutal hack - need to find a better way
-    killall tail
-    if [[ -e ${dialog_log} ]]; then
+    pgrep tail && pkill tail
+    if [[ -f "${dialog_log}" ]]; then
         update_log "removing ${dialog_log}"
 		rm "${dialog_log}"
     fi
+    update_log "removing $lock_file"
     rm -f "$lock_file"
+    update_log "***** End *****"
     exit 0
 }
 
 function read_jamf_log() {
-    update_log "Starting jamf log read"    
+    update_log "starting jamf log read"    
+    launchd_removal_count=0
     if [[ "${jamf_pid}" ]]; then
-        update_log "Processing jamf pro log for PID ${jamf_pid}"
+        update_log "processing jamf pro log for PID ${jamf_pid}"
         while read -r line; do    
             status_line=$(echo "${line}")
             case "${status_line}" in
@@ -64,7 +72,6 @@ function read_jamf_log() {
                         dialog_cmd "progresstext: Complete"
                         dialog_cmd "progress: complete"
                         sleep 2
-                        dialog_cmd "quit: "
                         update_log "Success Break"
                         quit_script
                     fi
@@ -78,20 +85,23 @@ function read_jamf_log() {
                         dialog_cmd "quit: "
                         dialog_error "${status_line}"
                         update_log "Error Break"
-                        #break
                         quit_script
                     fi
                 ;;
                 *"Removing existing launchd task"*)
-                    if [[ "${status_line}" == *"$jamf_pid"* ]]; then
+                    $(( launchd_removal_count++ ))
+                    if [[ ${launchd_removal_count} -ge 2 ]]; then
                         update_log "Launchd task removed"
-                        dialog_cmd "progresstext: Launchd task was removed"
+                        dialog_cmd "progresstext: Completed"
                         dialog_cmd "progress: complete"
                         sleep 2
-                        dialog_cmd "quit: "
                         update_log "Ambiguous Break"
-                        #break
                         quit_script
+                    else
+                        progresstext=$(echo "${status_line}" | awk -F "]: " '{print $NF}')
+                        update_log "Reading policy entry : ${progresstext}"
+                        dialog_cmd "progresstext: ${progresstext}"
+                        dialog_cmd "progress: increment"
                     fi
                 ;;
                 *"Executing Policy"*)
@@ -111,44 +121,61 @@ function read_jamf_log() {
             esac
             ((count++))
             if [[ ${count} -gt 10 ]]; then
-                update_log "Hit maxcount"
+                update_log "hit maxcount"
                 dialog_cmd "progress: complete"
                 sleep 0.5
                 #break
                 quit_script
             fi
         done < <(tail -f -n1 $jamf_log) 
-    else
-        update_log "Something went wrong"
-        echo "ok, something weird happened. We should have a PID but we don't."
     fi
-    update_log "End while loop"
+    update_log "end while loop"
 }
 
 function main() {
     update_log "***** Start *****"
+    last_log_entry=$( tail -n 1 "$jamf_log" )
+    update_log "$last_log_entry"
     if [[ -f "$lock_file" ]]; then
         # script is already running or crashed
+        update_log "$lock_file present so script is already running"
         exit
     fi
 
     # write lockfile to prevent this script doing anything twice
+    update_log "creating $lock_file"
     touch "$lock_file"
-    last_log_entry=$(tail -n 1 "$jamf_log")
-    if [[ "$last_log_entry" == *"Executing Policy"* ]]; then
-        update_log "Running launch_dialog function"
-        update_log "Getting Policy Name"
-        policy_name=$( sed 's|^.*Executing Policy ||' <<< "$last_log_entry" )
-        launch_dialog
-        update_log "Getting Policy ID"
+    if [[ "$last_log_entry" == *"Checking for policy ID"* ]]; then
+        update_log "getting PID"
         jamf_pid=$( awk -F"[][]" '{print $2}' <<< "$last_log_entry" )
         if [[ $jamf_pid ]]; then
-            update_log "Policy ID is ${jamf_pid}"
+            update_log "PID is ${jamf_pid}"
+        else
+            update_log "couldn't get a PID"
         fi
-        update_log "Processing Jamf Log"
-        read_jamf_log
+        update_log "getting policy name"
+        n=0
+        while [[ $n -lt 10 ]]; do
+            log_entry=$( tail -n 1 "$jamf_log" )
+            if [[ $log_entry != "$last_log_entry" ]]; then
+                if [[ "$log_entry" == *"Executing Policy"* ]]; then
+                    policy_name=$( sed 's|^.*Executing Policy ||' <<< "$log_entry" )
+                    break
+                fi
+                last_log_entry="$log_entry"
+                $(( n++ ))
+            fi
+            sleep 0.1
+        done
+        if [[ $policy_name ]]; then
+            update_log "policy name is $policy_name"
+            launch_dialog
+            update_log "Processing Jamf Log"
+            read_jamf_log
+        else
+            update_log "policy name not found"
+        fi
         update_log "All Done we think"
-        update_log "***** End *****"
     fi
     quit_script
 }
