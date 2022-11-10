47f99966fab248e34dd22ce037fed8dfd886c6fc
Graham Pugh <g.r.pugh@gmail.com>
2022-11-10 14:07:19 +0100

    fix script

--- Scripts_EAs/VersionStringOverrideFullPath-EA.sh
+++ Scripts_EAs/VersionStringOverrideFullPath-EA.sh
@@ -2,8 +2,8 @@
 string_type=%VERSION_STRING_TYPE%
 path_to_plist=%VERSION_PLIST%
 version_string="None"
-if [ -f "%VERSION_PLIST_PATH%" ]; then
-    version_string=$(defaults read "%VERSION_PLIST_PATH%" $string_type)
+if [ -f "$path_to_plist" ]; then
+    version_string=$(defaults read "$path_to_plist" $string_type)
 fi
 echo "<result>$version_string</result>"
 exit 0
