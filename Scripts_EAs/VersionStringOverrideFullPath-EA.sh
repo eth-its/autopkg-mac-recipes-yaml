920f09414b463ff85b93c5eb5102fe4e7a9e8991
Graham Pugh <g.r.pugh@gmail.com>
2022-11-15 10:03:03 +0100

    add path

--- Scripts_EAs/VersionStringOverrideFullPath-EA.sh
+++ Scripts_EAs/VersionStringOverrideFullPath-EA.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 string_type="%VERSION_STRING_TYPE%"
 path_to_plist="%VERSION_PLIST%"
 version_string="None"
-if [ -f "$path_to_plist" ]; then
-    version_string=$(defaults read "$path_to_plist" $string_type)
+if [[ -f "$path_to_plist" ]]; then
+    version_string=$(/usr/bin/defaults read "$path_to_plist" $string_type)
 fi
 echo "<result>$version_string</result>"
 exit 0
