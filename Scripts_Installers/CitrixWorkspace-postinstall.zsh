fdc1fdf048c40be6b2dba1bd2e9d17a025da5ca2
Graham Pugh <noemail@itscdgpmacmini>
2022-12-01 10:57:15 +0100

    add paths to binaries

--- Scripts_Installers/CitrixWorkspace-postinstall.zsh
+++ Scripts_Installers/CitrixWorkspace-postinstall.zsh
@@ -4,17 +4,17 @@
 # Turn off the Citrix Workspace Helper Tool
 
 # kill the various processes
-if pgrep "Citrix Workspace"; then
-    pkill "Citrix Workspace"
+if /usr/bin/pgrep "Citrix Workspace"; then
+    /usr/bin/pkill "Citrix Workspace"
 fi
 
-if pgrep "AuthManager_Mac"; then
-    pkill "AuthManager_Mac"
+if /usr/bin/pgrep "AuthManager_Mac"; then
+    /usr/bin/pkill "AuthManager_Mac"
 fi
 
 # current logged-in user's short name
 current_user=$(/bin/ls -la /dev/console | /usr/bin/cut -d " " -f 4)
-uid=$(id -u "$current_user")
+uid=$(/usr/bin/id -u "$current_user")
 echo "Current user is $current_user"
 
 # set preference to remove the helper tool
