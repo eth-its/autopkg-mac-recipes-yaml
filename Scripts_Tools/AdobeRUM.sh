01dc90cca84c4e1c84cc5cdad5dd93bde64c20e4
Graham Pugh <g.r.pugh@gmail.com>
2023-03-20 11:38:38 +0100

    creative cloud installation/update policies

--- Scripts_Tools/AdobeRUM.sh
+++ Scripts_Tools/AdobeRUM.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # run Adobe Remote Update Manager if installed; install if not present
+# this should be scoped only to clients that have Adobe CC apps installed
+
+# install if not present
 if [[ ! -f '/usr/local/bin/RemoteUpdateManager' ]]; then
     jamf policy -event AdobeRUM-install
 fi
 
-# this should be scoped only to clients that have Adobe CC apps installed
+# now run
 if [[ -f '/usr/local/bin/RemoteUpdateManager' ]]; then
     /usr/local/bin/RemoteUpdateManager --action=install
 else
