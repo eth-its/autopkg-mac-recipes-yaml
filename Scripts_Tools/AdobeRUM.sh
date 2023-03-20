77313f047961428d7c90c1ebc88d5fd8042a3683
Graham Pugh <g.r.pugh@gmail.com>
2023-03-20 10:50:31 +0100

    policies to install Adobe RUM

--- Scripts_Tools/AdobeRUM.sh
+++ Scripts_Tools/AdobeRUM.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# run Adobe Remote Update Manager if installed
-# this should be scoped only to clients that have Adobe CC SDL apps installed
+# run Adobe Remote Update Manager if installed; install if not present
+if [[ ! -f '/usr/local/bin/RemoteUpdateManager' ]]; then
+    jamf policy -event AdobeRUM-install
+fi
+
+# this should be scoped only to clients that have Adobe CC apps installed
 if [[ -f '/usr/local/bin/RemoteUpdateManager' ]]; then
     /usr/local/bin/RemoteUpdateManager --action=install
 else
-    echo 'ERROR: Adobe remote update manager not installed.' 1>&2
+    echo 'ERROR: Adobe Remote Update Manager not installed.' 1>&2
     exit 1
 fi
-
-# don't report failure if some apps fail to install
-exit 0
 
