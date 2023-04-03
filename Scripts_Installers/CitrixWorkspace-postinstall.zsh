ccf6c2058b6a2ff3a49c1dd9e07be3878129baa8
Graham Pugh <g.r.pugh@gmail.com>
2023-04-03 19:58:59 +0200

    remove the launchagents that cause the app to autolaunch

--- Scripts_Installers/CitrixWorkspace-postinstall.zsh
+++ Scripts_Installers/CitrixWorkspace-postinstall.zsh
@@ -15,10 +15,44 @@ fi
 # current logged-in user's short name
 current_user=$(/bin/ls -la /dev/console | /usr/bin/cut -d " " -f 4)
 uid=$(/usr/bin/id -u "$current_user")
-echo "Current user is $current_user"
+echo "Current user is $current_user (ID $uid)"
 
 # set preference to remove the helper tool
 /usr/bin/defaults write "/Users/$current_user/Library/Preferences/com.citrix.receiver.nomas.plist" ShowHelperInMenuBar 0
 /usr/sbin/chown ${current_user}:staff "/Users/$current_user/Library/Preferences/com.citrix.receiver.nomas.plist"
+
+# remove the launchagents to prevent launching at startup
+
+helper_agent="com.citrix.ReceiverHelper"
+
+if /bin/launchctl list "$helper_agent"; then
+    if /bin/launchctl bootout gui/$uid/$helper_agent; then
+        echo "ReceiverHelper LaunchAgent successfully unloaded"
+    else
+        echo "ReceiverHelper LaunchAgent was not unloaded."
+    fi
+
+    if /bin/rm "/Library/LaunchAgents/$helper_agent.plist"; then
+        echo "ReceiverHelper LaunchAgent successfully deleted"
+    else
+        echo "ReceiverHelper LaunchAgent was not deleted."
+    fi
+fi
+
+web_agent="com.citrix.Weblauncher"
+
+if /bin/launchctl list "$web_agent"; then
+    if /bin/launchctl bootout gui/$uid/$web_agent; then
+        echo "WebLauncher LaunchAgent successfully unloaded"
+    else
+        echo "WebLauncher LaunchAgent was not unloaded."
+    fi
+
+    if /bin/rm "/Library/LaunchAgents/$web_agent.plist"; then
+        echo "WebLauncher LaunchAgent successfully deleted"
+    else
+        echo "WebLauncher LaunchAgent was not deleted."
+    fi
+fi
 
 echo "Written preferences to /Users/$current_user/Library/Preferences/com.citrix.receiver.nomas.plist"
