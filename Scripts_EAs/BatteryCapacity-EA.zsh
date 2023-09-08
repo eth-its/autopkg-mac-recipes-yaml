401f50a379cdab3809693562025345e9d232f1b2
Graham Pugh <g.r.pugh@gmail.com>
2023-09-08 11:42:55 +0200

    change so it works on intel

--- Scripts_EAs/BatteryCapacity-EA.zsh
+++ Scripts_EAs/BatteryCapacity-EA.zsh
@@ -2,15 +2,18 @@
 # shellcheck shell=bash
 
 # Determine maximum battery capacity and state if degraded (under 80%)
+arch=$(/usr/bin/arch)
 
-capacity=$(system_profiler SPPowerDataType | sed -n -e 's/^.*Maximum Capacity: //p' | sed 's/\%//')
+condition=$(system_profiler SPPowerDataType | grep "Condition:" | sed 's/.*Condition: //')
 
-if [[ ! $capacity ]]; then
+if [[ "$arch" == "arm64" ]]; then
+    capacity=" ($(system_profiler SPPowerDataType | grep "Maximum Capacity:" | sed 's/.*Maximum Capacity: //')%)"
+fi
+
+if [[ ! $condition ]]; then
     echo "<result>No battery</result>"
-elif [[ $capacity -lt 80 ]]; then
-    echo "<result>$capacity - degraded</result>"
 else
-    echo "<result>$capacity</result>"
+    echo "<result>$condition</result>"
 fi
 
 exit 0
