c74f2ea78aab4f6cc5d958ed956d723886af3ec4
Graham Pugh <g.r.pugh@gmail.com>
2023-09-12 14:36:11 +0200

    update entire script

--- Scripts_Uninstallers/MindManager-uninstall.sh
+++ Scripts_Uninstallers/MindManager-uninstall.sh
@@ -6,86 +6,83 @@
 #
 #######################################################################
 
-function silent_app_quit() {
+silent_app_quit() {
     # silently kill the application.
-    # remove .app from end of string if supplied
-    app_name="${1/.app/}"
-    if [[ $(pgrep -ix "$app_name") ]]; then
-    	echo "Closing $app_name"
-    	/usr/bin/osascript -e "quit app \"$app_name\""
-    	sleep 1
+    # add .app to end of string if not supplied
+    app_name="${app_name/\.app/}"            # remove any .app
+    check_app_name="${app_name/\(/\\(}"       # escape any brackets for the pgrep
+    check_app_name="${check_app_name/\)/\\)}"  # escape any brackets for the pgrep
+    check_app_name="${check_app_name}.app"     # add the .app back
+    if pgrep -f "/${check_app_name}" ; then
+        echo "Closing $check_app_name"
+        /usr/bin/osascript -e "quit app \"$app_name\"" &
+        sleep 1
 
-    	# double-check
-    	countUp=0
-    	while [[ $countUp -le 10 ]]; do
-    		if [[ -z $(pgrep -ix "$app_name") ]]; then
-    			echo "$app_name closed."
-    			break
-    		else
-    			(( countUp++ ))
-    			sleep 1
-    		fi
-    	done
-        if [[ $(pgrep -x "$app_name") ]]; then
-    	    echo "$app_name failed to quit - killing."
-    	    /usr/bin/pkill "$app_name"
+        # double-check
+        n=0
+        while [[ $n -lt 10 ]]; do
+            if pgrep -f "/$check_app_name" ; then
+                (( n=n+1 ))
+                sleep 1
+				echo "Graceful close attempt # $n"
+            else
+                echo "$app_name closed."
+                break
+            fi
+        done
+        if pgrep -f "/$check_app_name" ; then
+            echo "$check_app_name failed to quit - killing."
+            /usr/bin/pkill -f "/$check_app_name"
         fi
     fi
 }
 
-function delete_app() {
-    # Now remove the app
-    echo "Removing application: ${app_name}"
-
-    # Add .app to end when providing just a name e.g. "TeamViewer"
-    if [[ ! $app_name == *".app"* ]]; then
-        app_name=$app_name".app"
-    fi
-
-    # Add standard path if none provided
-    if [[ ! $app_name == *"/"* ]]; then
-        appToDelete="/Applications/$app_name"
-    else
-        appToDelete="$app_name"
-    fi
-
-    # Remove the application
-    if [[ -d "${appToDelete}" ]]; then
-        echo "Application will be deleted: $appToDelete"
-        /bin/rm -Rf "${appToDelete}"
-        echo "Checking if $app_name is actually deleted..."
-        if [[ -d "${appToDelete}" ]]; then
-            echo "$app_name failed to delete"
-        else
-            echo "$app_name deleted successfully"
-        fi
-    else
-        echo "Application not present: $appToDelete"
-    fi
-
-    # also check to see if an additional app was ever created due to BundleID mismatch
-    if [[ -d "/Applications/${app_name}/${app_name}.app" ]]; then
-        echo "Folder will be deleted: /Applications/${app_name}/"
-        /bin/rm -Rf "/Applications/${app_name}" ||:
-    else
-        echo "Folder not found: /Applications/${app_name}/"
-    fi
-    if [[ -d "/Applications/${app_name}.localized/${app_name}.app" ]]; then
-        echo "Folder will be deleted: /Applications/${app_name}.localized/"
-        /bin/rm -Rf "/Applications/${app_name}.localized" ||:
-    else
-        echo "Folder not found: /Applications/${app_name}.localized/"
-    fi
-}
-
+# Inputted variables
 app_name="MindManager"
-silent_app_quit "$app_name"
-delete_app "$app_name"
 
-app_name="Mindjet MindManager.app"
-silent_app_quit "$app_name"
-delete_app "$app_name"
+if [[ -z "${app_name}" ]]; then
+    echo "No application specified!"
+    exit 1
+fi
 
+# quit the app if running
+silent_app_quit "$app_name"
+
+
+# Now remove the app
+echo "Removing application: ${app_name}"
+
+# Add standard path if none provided
+if [[ ! $app_name == *"/"* ]]; then
+	app_to_trash="/Applications/$app_name.app"
+else
+	app_to_trash="$app_name.app"
+fi
+
+echo "Application will be deleted: $app_to_trash"
+# Remove the application
+/bin/rm -Rf "${app_to_trash}"
+
+echo "Checking if $app_name is actually deleted..."
+if [[ -d "${app_to_trash}" ]]; then
+    echo "$app_name failed to delete"
+else
+    echo "$app_name deleted successfully"
+fi
+
+# also check to see if an additional app was ever created due to BundleID mismatch
+if [[ -d "/Applications/${app_name}/${app_name}.app" ]]; then
+    echo "Folder will be deleted: /Applications/${app_name}/"
+    /bin/rm -Rf "/Applications/${app_name}" ||:
+else
+    echo "Folder not found: /Applications/${app_name}/"
+fi
+if [[ -d "/Applications/${app_name}.localized/${app_name}.app" ]]; then
+    echo "Folder will be deleted: /Applications/${app_name}.localized/"
+    /bin/rm -Rf "/Applications/${app_name}.localized" ||:
+else
+    echo "Folder not found: /Applications/${app_name}.localized/"
+fi
 
 # Try to Forget the packages if we can find a match
 for pkg in $(/usr/sbin/pkgutil --pkgs=com.mindjet.mindmanager.*); do
