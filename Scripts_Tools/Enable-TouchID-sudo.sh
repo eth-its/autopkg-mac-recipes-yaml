67720ede6a90bc3f3ce2d15f2df0bbece4764215
Graham Pugh <gpugh@ethz.ch>
2023-09-27 16:36:38 +0200

    updated scripts

--- Scripts_Tools/Enable-TouchID-sudo.sh
+++ Scripts_Tools/Enable-TouchID-sudo.sh
@@ -1,7 +1,25 @@
-﻿#!/bin/bash
-# enable touch id for sudo
-# from https://github.com/flammable/enable_sudo_touchid/blob/master/munki_postinstall_script.sh
+#!/bin/zsh
+# shellcheck shell=bash
+
+: <<DOC
+Enable TouchID for sudo
+from https://github.com/flammable/enable_sudo_touchid/blob/master/munki_postinstall_script.sh
+and https://sixcolors.com/post/2023/08/in-macos-sonoma-touch-id-for-sudo-can-survive-updates/
+DOC
 
 enable_touchid="auth       sufficient     pam_tid.so"
 
-/usr/bin/sed -i '' -e "1s/^//p; 1s/^.*/${enable_touchid}/" /etc/pam.d/sudo
+# check for sonoma
+if [[ -f /etc/pam.d/sudo_local.template ]]; then
+
+    # check for an existing local sudo file
+    if [[ ! -f /etc/pam.d/sudo_local ]]; then
+        cp /etc/pam.d/sudo_local.template /etc/pam.d/sudo_local
+    fi
+
+    # ensure the touchid line is not commented out
+    /usr/bin/sed -i '' -e "s/\#${enable_touchid}/${enable_touchid}/" /etc/pam.d/sudo_local
+else
+    # method for older OS
+    /usr/bin/sed -i '' -e "1s/^//p; 1s/^.*/${enable_touchid}/" /etc/pam.d/sudo
+fi
