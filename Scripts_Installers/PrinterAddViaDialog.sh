80c50d6f029061e6a66e70c34efd286eee4c56e3
Graham Pugh <g.r.pugh@gmail.com>
2023-10-18 20:25:01 +0200

    Improvements

--- Scripts_Installers/PrinterAddViaDialog.sh
+++ Scripts_Installers/PrinterAddViaDialog.sh
@@ -12,6 +12,8 @@ PRINTER_DNS_ADDRESS="$4"
 # Provide any optional printer defaults that need set as parameter 5 variable - get these with lpoptions -p $PRINTER_PPD_NAME -l
 PRINTER_DEFAULT_OPTIONS="$5"
 
+# Queue name is the bonjour name with dashes replaced by underscores.
+PRINTER_PPD_NAME=$(basename "$PRINTER_DNS_ADDRESS" | sed 's|-|_|g')
 
 # Timeout timer
 TIMER=0
@@ -20,27 +22,35 @@ CURRENT_USER=$(scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /
 USER_ID=$(/usr/bin/id -u "$CURRENT_USER")
 PRINTER_ICON="/System/Library/CoreServices/AddPrinter.app/Contents/Resources/Printer.icns"
 JAMF_HELPER="/Library/Application Support/JAMF/bin/jamfHelper.app/Contents/MacOS/jamfHelper"
-DESCRIPTION="Adding $PRINTER_DNS_ADDRESS failed due to communication error. Please make sure you are on the correct wireless network and try again. 
+
+# Dialog to show when installation failed
+FAILED_TITLE="Adding Printer '$PRINTER_PPD_NAME' Failed"
+FAILED_DIALOG="Adding $PRINTER_PPD_NAME failed due to communication error. Please make sure you are on the correct wireless network and try again. 
 
 If it fails a second time, please contact the IT Dept for assistance."
-WAIT="Please wait a few moments while we remove the existing printer.
 
-This dialogue window will close itself when we are ready to proceed."
+# Dialog to show when reinstalling an existing printer
+REINSTALL_TITLE="Reinstalling Printer '$PRINTER_PPD_NAME'"
+REINSTALL_DIALOG="An existing print queue named '$PRINTER_PPD_NAME' has been detected. Please wait a few moments while we remove it so that it can be reinstalled.
+
+This dialogue window will close itself when we are ready to proceed. You will be asked to press 'Continue' to complete the installation."
+
+# Dialog to show when installing a new printer
+ADD_TITLE="Adding Printer '$PRINTER_PPD_NAME'"
+ADD_DIALOG="Please press OK to begin the print queue installation process. You will be asked to press 'Continue' to complete the installation."
 BUTTON1="OK"
 
-# Queue name is the bonjour name with dashes replaced by underscores.
-PRINTER_PPD_NAME=$(basename "$PRINTER_DNS_ADDRESS" | sed 's|-|_|g')
 
-if [ -f "/private/etc/cups/ppd/${PRINTER_PPD_NAME}.ppd" ] 
-then
+if [[ -f "/private/etc/cups/ppd/${PRINTER_PPD_NAME}.ppd" ]]; then
     echo "Printer already exists delete it first to avoid duplicate"
     /usr/sbin/lpadmin -x "${PRINTER_PPD_NAME}"
     # /usr/sbin/lpadmin -x "${PRINTER_PPD_NAME}___Fax"
-    /bin/launchctl asuser "$USER_ID" /usr/bin/sudo -u "$CURRENT_USER" "$JAMF_HELPER" -windowType utility -title "Removing Existing Printer" -description "$WAIT" -icon "$PRINTER_ICON" -button1 "$BUTTON1" -defaultButton "1" &
+    /bin/launchctl asuser "$USER_ID" /usr/bin/sudo -u "$CURRENT_USER" "$JAMF_HELPER" -windowType utility -title "$REINSTALL_TITLE" -description "$REINSTALL_DIALOG" -icon "$PRINTER_ICON" -button1 "$BUTTON1" -defaultButton "1" &
     sleep 10
     pkill jamfHelper
 else
-   echo "Printer does NOT already exist so nothing to delete"
+    echo "Printer does NOT already exist so nothing to delete"
+    /bin/launchctl asuser "$USER_ID" /usr/bin/sudo -u "$CURRENT_USER" "$JAMF_HELPER" -windowType utility -title "$ADD_TITLE" -description "$ADD_DIALOG" -icon "$PRINTER_ICON" -button1 "$BUTTON1" -defaultButton "1" &
 fi
 
 # Open the AddPrinter.app and pass the URL to the printer to add the device
@@ -49,23 +59,22 @@ open -a /System/Library/CoreServices/AddPrinter.app "$PRINTER_DNS_ADDRESS"
 
 while [[ ! -f "/private/etc/cups/ppd/${PRINTER_PPD_NAME}.ppd" && TIMER -lt 20 ]] # wait for ppd file to be written to disk
 do
-   /bin/sleep 5
-   TIMER=$((TIMER + 5))
-   echo $TIMER
+    /bin/sleep 5
+    TIMER=$((TIMER + 5))
+    echo $TIMER
 done
 
-if [[ -f "/private/etc/cups/ppd/${PRINTER_PPD_NAME}.ppd" ]]
-then
-   if [ -n "$PRINTER_DEFAULT_OPTIONS" ] # Check if any default options are passed, if there are set them
-   then
-      /usr/sbin/lpadmin -p "$PRINTER_PPD_NAME" "$PRINTER_DEFAULT_OPTIONS"
-      echo "Printer ${PRINTER_DNS_ADDRESS} added using queue name ${PRINTER_PPD_NAME} and default options of ${PRINTER_DEFAULT_OPTIONS} set"
-   else
-      echo "Printer ${PRINTER_DNS_ADDRESS} added using queue name ${PRINTER_PPD_NAME}"
-   fi
+if [[ -f "/private/etc/cups/ppd/${PRINTER_PPD_NAME}.ppd" ]]; then
+    # Check if any default options are passed, if there are set them
+    if [ -n "$PRINTER_DEFAULT_OPTIONS" ]; then
+        /usr/sbin/lpadmin -p "$PRINTER_PPD_NAME" "$PRINTER_DEFAULT_OPTIONS"
+        echo "Printer ${PRINTER_DNS_ADDRESS} added using queue name ${PRINTER_PPD_NAME} and default options of ${PRINTER_DEFAULT_OPTIONS} set"
+    else
+        echo "Printer ${PRINTER_DNS_ADDRESS} added using queue name ${PRINTER_PPD_NAME}"
+    fi
 else
-   echo "Printer PPD file not created for some reason"
-   pgrep AddPrinter && pkill AddPrinter
-   /bin/launchctl asuser "$USER_ID" /usr/bin/sudo -u "$CURRENT_USER" "$JAMF_HELPER" -windowType utility -windowPosition lr -title "Add Printer Failed" -description "$DESCRIPTION" -icon "$PRINTER_ICON" -button1 "$BUTTON1" -defaultButton "1"
-   exit 1
+    echo "Printer PPD file not created for some reason"
+    pgrep AddPrinter && pkill AddPrinter
+    /bin/launchctl asuser "$USER_ID" /usr/bin/sudo -u "$CURRENT_USER" "$JAMF_HELPER" -windowType utility -windowPosition lr -title "$FAILED_TITLE" -description "$FAILED_DIALOG" -icon "$PRINTER_ICON" -button1 "$BUTTON1" -defaultButton "1"
+    exit 1
 fi
