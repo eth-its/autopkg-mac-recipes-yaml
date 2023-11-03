ce5fce7333360225e0725c96ca26c08c00355329
andreilabin <132457301+andreilabin@users.noreply.github.com>
2023-11-03 11:09:43 +0100

    amend rm for config-file

--- Scripts_Installers/Prism-postinstall.sh
+++ Scripts_Installers/Prism-postinstall.sh
@@ -2,11 +2,8 @@
 
 # Deletes existing configuration and license-file. This is only needed transitioning from Prism 9 to Prism 10
 
-config_path="/Library/Application Support/GraphPad/Prism"
-config_file="$config_path/prism-config.xml"
-
 # Delete configuration file from main Library
-rm -f $config_file
+rm -f "/Library/Application Support/GraphPad/Prism/prism-config.xml"
 
 # Delete license file from logged-in user
 targetUser=$(who | grep console | awk '{ print $1 }')
