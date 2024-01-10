44ed0082bcd5732f4aab79b9583ac7821ff68113
katidzehnder <78544227+katidzehnder@users.noreply.github.com>
2024-01-10 13:23:35 +0100

    Update GLPIAgent-EA.sh
    package identifier changed

--- Scripts_EAs/GLPIAgent-EA.sh
+++ Scripts_EAs/GLPIAgent-EA.sh
@@ -1,6 +1,8 @@
 #!/bin/bash 
 
-if pkgutil --pkgs=org.glpi-project.glpi-agent; then
+if pkgutil --pkgs=com.teclib.glpi-agent; then
+    glpi_agent_version=$(pkgutil --pkg-info com.teclib.glpi-agent | grep version | sed 's|version: ||')
+elif pkgutil --pkgs=org.glpi-project.glpi-agent; then
     glpi_agent_version=$(pkgutil --pkg-info org.glpi-project.glpi-agent | grep version | sed 's|version: ||')
 else
     glpi_agent_version="None"
