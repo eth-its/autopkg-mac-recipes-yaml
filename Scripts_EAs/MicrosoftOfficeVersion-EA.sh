0bb3944ee96fc251c48a16de482c975f05ad2161
andreilabin <132457301+andreilabin@users.noreply.github.com>
2024-02-26 14:56:52 +0100

    amend for EA (current version installed)

---
+++ Scripts_EAs/MicrosoftOfficeVersion-EA.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+APP="/Applications/Microsoft Word.app"
+if [[ ! -e "$APP" ]]; then
+    echo "$APP is not installed."
+    oldest_version_full="0"
+else 
+    myWord_full=$(defaults read "/Applications/Microsoft Word.app/Contents/Info.plist" CFBundleVersion )
+    myExcel_full=$(defaults read "/Applications/Microsoft Excel.app/Contents/Info.plist" CFBundleVersion )
+    myPowerPoint_full=$(defaults read "/Applications/Microsoft PowerPoint.app/Contents/Info.plist" CFBundleVersion )
+    myOutlook_full=$(defaults read "/Applications/Microsoft Outlook.app/Contents/Info.plist" CFBundleVersion )
+    myOneNote_full=$(defaults read "/Applications/Microsoft OneNote.app/Contents/Info.plist" CFBundleVersion )
+    oldest_version_full=$(echo "$myWord_full" "$myExcel_full" "$myPowerPoint_full" "$myOutlook_full" "$myOneNote_full" | tr ' ' '\n' | sort | head -n 1 )
+fi
+echo "<result>$oldest_version_full</result>"
+
+exit 0
