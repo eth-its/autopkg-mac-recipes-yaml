eb5172588cb11de50cfa8d3b7dacc2a8e8de18ec
mmanivannan <mathushanth.manivannan@ethz.ch>
2024-03-15 10:45:00 +0100

    switched to zsh instead of bash

---
+++ Scripts_Uninstallers/AdobeObsoleteApps-uninstall.zsh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+# script to delete obsolete Adobe Apps
+for app_dir in /Applications/Adobe*/; do
+    app_name=$(basename "$app_dir")
+    if ! echo "$app_name" | egrep -q "Acrobat|Creative|2023|2024|2025"; then
+        rm -rf "$app_dir"
+        echo "Deleting $app_name"
+        echo "Checking if $app_name is actually deleted..."
+        if [[ -d "${app_dir}" ]]; then
+            echo "$app_name failed to delete"
+        else
+            echo "$app_name deleted successfully"
+        fi
+    fi
+done
+
+exit 0
