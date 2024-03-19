a33d3e83c17d723748f4bc6c4522b1f3122daeb1
andreilabin <132457301+andreilabin@users.noreply.github.com>
2024-03-19 09:42:12 +0100

    add rush to ea exclusion

--- Scripts_EAs/AdobeObsoleteApps.zsh
+++ Scripts_EAs/AdobeObsoleteApps.zsh
@@ -6,6 +6,6 @@ Lists obsolete Adobe Apps. Exclusions can be edited for e.g. Creative Cloud or A
 of the titles we want to keep).
 
 DOC
-echo "<result>$(find /Applications/Adobe* -print -maxdepth 0 | egrep -v  "Acrobat|Creative|2023|2024|2025")</result>"
+echo "<result>$(find /Applications/Adobe* -print -maxdepth 0 | egrep -v  "Acrobat|Creative|2023|2024|2025|Rush")</result>"
 
 exit 0
