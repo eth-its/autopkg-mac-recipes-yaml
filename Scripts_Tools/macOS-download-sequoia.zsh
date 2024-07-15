d040aaede1f17887b4650c78f4c56003d8098c35
Kati Zehnder <katiuscia.zehnder@id.ethz.ch>
2024-07-15 17:38:43 +0200

    adding Sequoia

---
+++ Scripts_Tools/macOS-download-sequoia.zsh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+# shellcheck shell=bash
+
+: <<DOC 
+erase-install run script for downloading the latest available Install macOS Sonoma.app
+by Graham Pugh
+DOC
+
+jamf policy -event "erase-install-install"
+
+/Library/Management/erase-install/erase-install.sh --os 15 --update
+
