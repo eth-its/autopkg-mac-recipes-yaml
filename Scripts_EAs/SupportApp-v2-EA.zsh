4fc910b037358587e1db19ef17216390c49479dd
andreilabin <132457301+andreilabin@users.noreply.github.com>
2024-11-25 16:28:58 +0100

    EA for checking if new Support App Config has been run

---
+++ Scripts_EAs/SupportApp-v2-EA.zsh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+# shellcheck shell=bash
+
+# Support App Config version finder extension attribute
+
+version="None"
+
+# look for the CSDS folder
+if [[ -f "//Library/Management/ETHZ/SupportApp/status_update.zsh" ]]; then
+    # now look for the version_info file in the GOLD folder
+    echo "<result>new</result>"
+    else "<result>old</result>"
+fi
+
+
+exit 0
