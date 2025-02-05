caf666d381a47b29c7c8a3c83bb74b5f4db79e6c
andreilabin <132457301+andreilabin@users.noreply.github.com>
2025-02-05 17:08:59 +0100

    include killing the process

--- Scripts_Uninstallers/AtegraWebFileHandler-uninstall.sh
+++ Scripts_Uninstallers/AtegraWebFileHandler-uninstall.sh
@@ -9,6 +9,44 @@
 APP_PATH1="/Applications/ATEGRA Web File Handler.app"
 APP_PATH2="/Applications/ATEGRA Web File Handler Starter.app"
 
+function silent_app_quit() {
+    # silently kill the application.
+    # add .app to end of string if not supplied
+    app_name="${app_name/\.app/}"            # remove any .app
+    check_app_name="${app_name/\(/\\(}"       # escape any brackets for the pgrep
+    check_app_name="${check_app_name/\)/\\)}"  # escape any brackets for the pgrep
+    check_app_name="${check_app_name}.app"     # add the .app back
+    if pgrep -f "/${check_app_name}" ; then
+        echo "Closing $app_name"
+        /usr/bin/osascript -e "quit app \"$app_name\"" &
+        sleep 1
+
+        # double-check
+        n=0
+        while [[ $n -lt 10 ]]; do
+            if pgrep -f "$check_app_name" ; then
+                (( n=n+1 ))
+                sleep 1
+				echo "Graceful close attempt # $n"
+            else
+                echo "$app_name closed."
+                break
+            fi
+        done
+        if pgrep -f "$check_app_name" ; then
+            echo "$app_name failed to quit - killing."
+            /usr/bin/pkill -f "$check_app_name"
+        fi
+    fi
+}
+
+# MAIN
+
+app_name="ATEGRA Web File Handler"
+
+# quit the app if running
+silent_app_quit "$app_name"
+
 #Uninstall Ategra Web File Handler
 
 rm -rf "$APP_PATH1"
