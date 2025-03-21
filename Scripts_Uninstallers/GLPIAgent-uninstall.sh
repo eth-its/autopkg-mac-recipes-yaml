f9fd6a4e6c81aee764b48659744cb49f430cd92a
mathu-eth <mmanivannan@ethz.ch>
2025-03-21 14:11:31 +0100

    changing the uninstall script to remove uneccesary parts and adding glpi to the managed login items

--- Scripts_Uninstallers/GLPIAgent-uninstall.sh
+++ Scripts_Uninstallers/GLPIAgent-uninstall.sh
@@ -17,20 +17,4 @@ else
     echo "GLPI-Agent uninstaller script failed"
 fi
 
-# Forget packages
-echo "Forgetting packages"
-pkgutilcmd="/usr/sbin/pkgutil"
-$pkgutilcmd --pkgs=org.glpi-project.glpi-agent && $pkgutilcmd --forget org.glpi-project.glpi-agent
-$pkgutilcmd --pkgs=com.teclib.glpi-agent && $pkgutilcmd --forget com.teclib.glpi-agent
-
-echo "Stopping and unloading service"
-
-launchctl stop org.glpi-project.glpi-agent
-launchctl unload /Library/LaunchDaemons/org.glpi-project.glpi-agent.plist
-rm /Library/LaunchDaemons/org.glpi-project.glpi-agent.plist
-launchctl stop com.teclib.glpi-agent
-launchctl unload /Library/LaunchDaemons/com.teclib.glpi-agent.plist
-rm /Library/LaunchDaemons/com.teclib.glpi-agent.plist
-
-
 echo "GLPI-Agent deletion complete"
