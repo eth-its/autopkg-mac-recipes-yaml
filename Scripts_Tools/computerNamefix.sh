9a93a3f63c02dcff81a7995f5986e8c11eddec71
Kati Zehnder <katiuscia.zehnder@id.ethz.ch>
2025-11-10 11:14:33 +0100

    computerNamefix added

---
+++ Scripts_Tools/computerNamefix.sh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+#########################################################
+# Auto rename HostName and LocalHostName to ComputerName
+# created by Philippe Scholl
+# version 1.0.3
+# copyright by ethOS Mac Product Center
+#########################################################
+
+computerName="$2"
+
+if [[ -z "$computerName" ]]; then
+  echo "Error: No computer name provided."
+  exit 1
+fi
+
+if jamf setComputerName -name "$computerName"; then
+  echo "Successfully changed Computer Name to $computerName"
+  # Inventar in Jamf Pro aktualisieren
+  jamf recon >/dev/null 2>&1 || echo "Note: recon failed (non-fatal)."
+else
+  echo "Failed to change Computer Name!"
+  exit 1
+fi
