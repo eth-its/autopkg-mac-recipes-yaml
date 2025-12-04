f0f264debc0ea812aa0c8d4d9ffd331fe6558ceb
cglattfelder <christian.glattfelder@ethz.ch>
2025-12-04 15:14:48 +0100

    AdobeObsoleteApps: Declare 2026 as recent enough, and 2023 as obsolete

--- Scripts_EAs/AdobeObsoleteApps.zsh
+++ Scripts_EAs/AdobeObsoleteApps.zsh
@@ -6,6 +6,6 @@ Lists obsolete Adobe Apps. Exclusions can be edited for e.g. Creative Cloud or A
 of the titles we want to keep).
 
 DOC
-echo "<result>$(find /Applications/Adobe* -print -maxdepth 0 | egrep -v  "Acrobat|Creative|Lightroom|2023|2024|2025|Rush 2|XD|Dimension|Dreamweaver 2021")</result>"
+echo "<result>$(find /Applications/Adobe* -print -maxdepth 0 | egrep -v  "Acrobat|Creative|Lightroom|2024|2025|2026|Rush 2|XD|Dimension|Dreamweaver 2021")</result>"
 
 exit 0
