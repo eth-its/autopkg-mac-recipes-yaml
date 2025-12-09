2d23d9ce6f2ecf5efa3a069efe1adf249ec2dce9
cglattfelder <christian.glattfelder@ethz.ch>
2025-12-09 10:29:50 +0100

    Comsol: Download dmg instead of mouting via http ; prevent system sleep during installation

--- Scripts_Installers/Comsol-install.zsh
+++ Scripts_Installers/Comsol-install.zsh
@@ -3,25 +3,43 @@
 COMSOL_DMG_URL="$4"
 DIR=/Library/Management/ETHZ/Comsol
 
+# download image to /tmp, 
+cd /tmp
+curlexit=1
+while [[ $curlexit -gt 0 ]] ; do
+echo "$(date) : Starting Image Download"
+curl -C - -O "$COMSOL_DMG_URL" 
+curlexit=$?
+if [[ $curlexit -gt 0 ]]; then echo "$(date) : Download interrupted, resuming.." ; fi
+done
+
+echo "$(date) : COMSOL image download successful"
+IMAGENAME="$(basename "$COMSOL_DMG_URL")"
+
 # attach main image
-echo "Mounting COMSOL image"
+echo "$(date) : Mounting COMSOL image"
 APPPATH="/Volumes/COMSOL_setup_dmg"
 mkdir -p "$APPPATH"
-hdiutil attach "$COMSOL_DMG_URL" -nobrowse -mountpoint "$APPPATH"
+hdiutil attach "$IMAGENAME" -nobrowse -mountpoint "$APPPATH"
 
 # find the setup script
 installer=$(find "$APPPATH" -maxdepth 1 -name "COMSOL*.app" | head -n 1)
 if [[ -d "$installer" ]]; then
-    echo "Installing $installer" 
+    echo "$(date) : Installing $installer" 
+    caffeinate & echo $! >/tmp/coffeepid #prevent machine sleep,until the installer has run out
     if "$installer/Contents/Resources/setup" -s "$DIR/setupconfig.ini"; then
-        echo "Installation succeeded"
+        echo $(date) : Installation succeeded"
         rm -rf "$DIR"
     else
-        echo "ERROR: installation failed"
+        echo "$(date) : ERROR: installation failed"
     fi
     hdiutil eject $APPPATH
+    rm -f /tmp/"$IMAGENAME"
+    pkill -F /tmp/coffeepid
+    rm -f /tmp/coffeepid
+    echo "$(date) : Image unmounted and deleted, exiting"
 else
-    echo "ERROR: Installer not found."
+    echo "$(date) : ERROR: Installer not found."
     hdiutil eject $APPPATH
     exit 1
 fi
