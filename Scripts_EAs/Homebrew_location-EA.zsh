ba84f7f0fb2a5aa7a14fc5edf87c2c2395810d8b
cglattfelder <christian.glattfelder@ethz.ch>
2026-02-27 10:28:54 +0100

    Homebrew: recreate EA to include ARM, and recreate installed Group to omit Users group

---
+++ Scripts_EAs/Homebrew_location-EA.zsh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+intel_brew="/usr/local/bin/brew"
+arm_brew="/opt/homebrew/bin/brew"
+
+if [ -f "$arm_brew" ]; then
+    result=$($arm_brew --prefix)
+elif [ -f "$intel_brew" ]; then
+    result=$($intel_brew --prefix)
+else
+    result="None"
+fi
+
+echo "<result>$result</result>”
