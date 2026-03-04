0f97a2c67102750af4d31572dc775d433add68cb
cglattfelder <christian.glattfelder@ethz.ch>
2026-03-04 12:46:23 +0100

    Homebrew: Homebrew_location becomes Homebrew_version

--- Scripts_EAs/Homebrew_version-EA.zsh
+++ Scripts_EAs/Homebrew_version-EA.zsh
@@ -4,11 +4,11 @@ intel_brew="/usr/local/bin/brew"
 arm_brew="/opt/homebrew/bin/brew"
 
 if [ -f "$arm_brew" ]; then
-    result=$($arm_brew --prefix)
+    result=$($arm_brew --version | tr -d 'Homebrew ')
 elif [ -f "$intel_brew" ]; then
-    result=$($intel_brew --prefix)
+    result=$($intel_brew --version | tr -d 'Homebrew ')
 else
-    result=""
+    result="None"
 fi
 
 echo "<result>$result</result>"
