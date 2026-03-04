6250f8a4b61f0219bd36fd369a700a4096adb6dd
cglattfelder <christian.glattfelder@ethz.ch>
2026-03-04 13:32:38 +0100

    Homebrew: Ask for version as user for EA ; deprecate old homebrew install recipe, introduce PKG based homebrew install recipe

--- Scripts_EAs/Homebrew_version-EA.zsh
+++ Scripts_EAs/Homebrew_version-EA.zsh
@@ -3,10 +3,12 @@
 intel_brew="/usr/local/bin/brew"
 arm_brew="/opt/homebrew/bin/brew"
 
+localusername=$(dscl . -search /Users UniqueID 501|awk '/UniqueID/ {print $1}')
+
 if [ -f "$arm_brew" ]; then
-    result=$($arm_brew --version | tr -d 'Homebrew ')
+    result=$(sudo -u $localusername zsh -c "$arm_brew --version | tr -d 'Homebrew '")
 elif [ -f "$intel_brew" ]; then
-    result=$($intel_brew --version | tr -d 'Homebrew ')
+    result=$(sudo -u $localusername zsh -c "$intel_brew --version | tr -d 'Homebrew '")
 else
     result="None"
 fi
