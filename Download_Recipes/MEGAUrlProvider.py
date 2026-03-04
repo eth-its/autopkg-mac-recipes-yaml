8c163166145e6dcf8d12257e4c3484a2d6959ea3
cglattfelder <christian.glattfelder@ethz.ch>
2026-03-04 10:38:50 +0100

    MEGA: Download custom processor now ensures no further downloads if it already has a pkg of the newest version number

--- Download_Recipes/MEGAUrlProvider.py
+++ Download_Recipes/MEGAUrlProvider.py
@@ -12,6 +12,7 @@ from __future__ import absolute_import, print_function
 import re
 from autopkglib import URLGetter
 import sys
+import os
 
 try:
     # import for Python 3
@@ -24,8 +25,6 @@ except ImportError:
 VERSION_URL = "https://www.megasoftware.net/history"
 BASE_URL = "https://www.megasoftware.net/do_force_download/MEGA_"
 REGEX = ".*MEGA\ \d{2}\ version\ (\d{2}\.\d\.\d)\ build .*"
-try: USER_AGENT=self.env["USER_AGENT"]
-except NameError: USER_AGENT = 'mozilla macintosh'
 
 # __all__ == ["MEGAURLProvider"]
 
@@ -34,14 +33,17 @@ class MEGAURLProvider(URLGetter):
 
     description = __doc__
     input_variables = {
-        "USER_AGENT": {"description": "User Agent used for downloading", "required":False}
+        "USER_AGENT": {"description": "User Agent used for downloading", "required":False, "default":"mozilla macintosh"}
     }
     output_variables = {
         "url": {"description": "URL to latest version"},
         "version": {"description": "The latest version found"},
+        "should_continue": {"description": "TRUEPREDICATE or FALSEPREDICATE depending on Cache directory contents"},
+        "MEGA_pkg_file_name": {"description": "filename of the pkg to be stored on disk"}
     }
 
     def main(self):
+        USER_AGENT=self.env["USER_AGENT"]
         if sys.version_info.major < 3:
             html_source = self.download(VERSION_URL,headers={'User-Agent':USER_AGENT})
         else:
@@ -52,6 +54,21 @@ class MEGAURLProvider(URLGetter):
         return_url = BASE_URL + unescaped_url + suffix
         self.env["version"] = escaped_url
         self.env["url"] = return_url
+
+        # if we already have a pkg for the current version, we should not re-download
+        # autopkg requires Content-Length, ETAG or Last-Modified headers, but MEGA's web server sends none of these. 
+        # if a previous download were incomplete, it would be stored as another/temporary filename, so this should be safeish.
+
+        MEGA_pkg_file_name = "MEGA_" + unescaped_url + suffix
+        working_directory = self.env["RECIPE_CACHE_DIR"] + "/downloads/"
+
+        if os.path.exists(working_directory + MEGA_pkg_file_name) :
+            self.env["should_continue"] = False
+        else:
+            self.env["should_continue"] = True
+
+        self.env["MEGA_pkg_file_name"] = MEGA_pkg_file_name
+
         print(
             "MEGAURLProvider: Match found is %s\n"
             "MEGAURLProvider: Unescaped url is: %s\n"
