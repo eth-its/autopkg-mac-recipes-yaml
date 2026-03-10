f1f91d21c98acfc7ad6bbbcd872c8d33e5f1bdf3
cglattfelder <christian.glattfelder@ethz.ch>
2026-03-10 18:51:21 +0100

    Homebrew: Uninstaller .. wait before waiting.

--- Scripts_Uninstallers/Homebrew-uninstall.sh
+++ Scripts_Uninstallers/Homebrew-uninstall.sh
@@ -17,8 +17,8 @@ chmod +x "$command_file"
 # run the command file as the console user
 sudo -u $consoleuser open "$command_file"
 
-# wait until the command file has finished
-sleep 2
+# wait until the command file has started, and then finished
+while [[ $(pgrep -f $command_file|wc -l) -eq 0 ]] ; do sleep 0.1 ; done 
 while [[ $(pgrep -f $command_file|wc -l) -gt 0 ]] ; do sleep 1 ; done 
 
 # if we can't see 'brew' anymore, the user has decided to remove homebrew - so we clean up a bit more
