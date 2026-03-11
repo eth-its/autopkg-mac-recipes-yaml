e7fbeca5df594fd863ed7beb6812e4725b7e49f6
cglattfelder <christian.glattfelder@ethz.ch>
2026-03-11 13:56:57 +0100

    Homebrew: Uninstall - don't ask the which ; just go and see for yourself whether you really should delete it all.

--- Scripts_Uninstallers/Homebrew-uninstall.sh
+++ Scripts_Uninstallers/Homebrew-uninstall.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-set -x
-
 # get console user so we can run the script as that user
 consoleuser=$(scutil <<< "show State:/Users/ConsoleUser" | awk '/Name :/ && ! /loginwindow/ { print $3 }')
 
@@ -18,17 +16,18 @@ chmod +x "$command_file"
 # run the command file as the console user
 sudo -u $consoleuser open "$command_file"
 
-# wait until the command file has started, and then finished
+# wait until the command file has started, and then stopped running ( things should be uninstalled by then )
 while [[ $(pgrep -f $command_file|wc -l) -eq 0 ]] ; do sleep 0.1 ; done 
 while [[ $(pgrep -f $command_file|wc -l) -gt 0 ]] ; do sleep 1 ; done 
 
 # if we can't see 'brew' anymore, the user has decided to remove homebrew - so we clean up a bit more
-which brew >/dev/null ; brewthere=$?
-if [[ $brewthere -eq 0 ]] ; then 
-pkgutil --forget sh.brew.homebrew /
-rm -rf /usr/local/Homebrew || true
-rm -rf /opt/homebrew || true
+if [[ ! -f /usr/local/bin/brew ]] && [[ ! -f /opt/homebrew/bin/brew ]] ; then
+    pkgutil --forget sh.brew.homebrew /  # to enable smooth re-installation for repentants
+    rm -rf /usr/local/Homebrew && echo "Removed directory /usr/local/Homebrew"
+    rm -rf /opt/homebrew && echo "Removed directory /opt/Homebrew"
+else 
+    echo "user decided to keep homebrew, exiting"
 fi
 
-# stop caffeinating
+# spill that other brew, too.
 kill "$caffeinatepid"
