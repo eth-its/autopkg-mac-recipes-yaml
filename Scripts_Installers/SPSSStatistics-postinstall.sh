ef627e516e3a551952b31da2d132539187166406
cglattfelder <christian.glattfelder@ethz.ch>
2026-03-23 16:50:11 +0100

    SPSS: backwards-compatible postinstall

--- Scripts_Installers/SPSSStatistics-postinstall.sh
+++ Scripts_Installers/SPSSStatistics-postinstall.sh
@@ -6,11 +6,16 @@ To continue to allow multiple versions to be installed concurrently, this script
 moves the folder to "IBM SPSS Statistics MAJOR_VERSION".
 DOC
 
+app_path=NOTSET
+if [[ -d "/Applications/IBM SPSS Statistics/IBM SPSS Statistics.app" ]]; then app_path="/Applications/IBM SPSS Statistics/IBM SPSS Statistics.app" ; 
+elif [[ -d "/Applications/IBM SPSS Statistics/SPSS Statistics.app" ]]; then app_path="/Applications/IBM SPSS Statistics/SPSS Statistics.app" ;
+else echo "No existing installation present within '/Applications/IBM SPSS Statistics'" ; exit 0 ; fi 
 
-if [[ -d "/Applications/IBM SPSS Statistics/SPSS Statistics.app" ]]; then
+
+if [[ app_path != "NOTSET" ]]; then
     echo "Moving IBM SPSS Statistics app folder"
     # check the actual version of the app inside the folder
-    installed_version=$(/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" "/Applications/IBM SPSS Statistics/IBM SPSS Statistics.app/Contents/Info.plist" | cut -d. -f1)
+    installed_version=$(/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" "$app_path/Contents/Info.plist" | cut -d. -f1)
 
     if ! mv "/Applications/IBM SPSS Statistics" "/Applications/IBM SPSS Statistics $installed_version"; then
         echo "ERROR: Failed to move folder"
