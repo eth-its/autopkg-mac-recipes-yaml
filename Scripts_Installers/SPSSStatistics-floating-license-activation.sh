9e99acec56c656908d8036de29307f2c78012c51
cglattfelder <christian.glattfelder@ethz.ch>
2026-03-23 17:50:25 +0100

    SPSS v30: licensing fun with app renames, and alos icons.

--- Scripts_Installers/SPSSStatistics-floating-license-activation.sh
+++ Scripts_Installers/SPSSStatistics-floating-license-activation.sh
@@ -9,7 +9,21 @@ if ! cd "/Applications/IBM SPSS Statistics %MAJOR_VERSION%/Resources/Activation"
     exit 1
 fi
 
+app_path=NOTSET
+if [[ -d "/Applications/IBM SPSS Statistics %MAJOR_VERSION%/IBM SPSS Statistics.app" ]]; then app_path="/Applications/IBM SPSS Statistics %MAJOR_VERSION%/IBM SPSS Statistics.app" ; 
+elif [[ -d "/Applications/IBM SPSS Statistics %MAJOR_VERSION%/SPSS Statistics.app" ]]; then app_path="/Applications/IBM SPSS Statistics %MAJOR_VERSION%/SPSS Statistics.app" ;
+else echo "No existing installation present within '/Applications/IBM SPSS Statistics'" ; exit 1 ; fi 
+
+installed_version=$(/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" "$app_path/Contents/Info.plist" | cut -d. -f1)
+
+# https://www.ibm.com/mysupport/s/defect/aCIKe000000g10Q/dt409141?language=en_US - app renames are hard. especially when the license team doesn't know about it ..
+if [[ $installed_version -eq 30 ]] ; then 
+mv "$app_path" "/Applications/IBM SPSS Statistics %MAJOR_VERSION%/SPSS Statistics.app"
 ./licenseactivator LSHOST=%FLOATING_LICENSE_URL% > /tmp/licenseactivator.txt
+mv "/Applications/IBM SPSS Statistics %MAJOR_VERSION%/SPSS Statistics.app" "$app_path"
+else
+./licenseactivator LSHOST=%FLOATING_LICENSE_URL% > /tmp/licenseactivator.txt
+fi
 
 if ! grep %FLOATING_LICENSE_URL% "/Applications/IBM SPSS Statistics %MAJOR_VERSION%/Resources/Activation/commutelicense.ini" ; then
     echo "ERROR: SPSS Statistics %MAJOR_VERSION% Floating License activation failed."
