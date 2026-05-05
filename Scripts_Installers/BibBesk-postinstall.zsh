236a2d7f41a8df7913f0cb5d99406f5650b6a47d
andreilabin <132457301+andreilabin@users.noreply.github.com>
2026-05-05 15:21:52 +0200

    first attempt at remoddeling bibdesk to the TeX-directory

---
+++ Scripts_Installers/BibBesk-postinstall.zsh
@@ -0,0 +1,59 @@
+#!/bin/zsh
+# shellcheck shell=bash
+
+autoload is-at-least
+
+:<<DOC
+MacTeX installs BibDesk.app into the /Applications/TeX folder,
+so we use an AutoPkg recipe that repackages BibDesk.app so that
+it is installed in that directory.
+
+But if somebody has an existing installation in /Applications,
+we end up with two versions.
+
+So, This postinstall script checks if there is an existing version
+which is older or the same as the version in the /Applications/TeX
+folder, and deletes it.
+DOC
+
+get_version() {
+    plist="$1/Contents/Info.plist"
+    version=$(/usr/libexec/PlistBuddy -c "Print :CFBundleShortVersionString" "$plist")
+    return $version
+}
+
+tex_location='/Applications/TeX/BibDesk.app'
+standalone_location='/Applications/BibDesk.app'
+
+# Check that the installation worked
+if [[ -d "$tex_location" ]]; then
+    tex_version=$(get_version "$tex_location")
+    echo "BibDesk.app installation found at $tex_location. Version: $tex_version"
+else
+    echo "BibDesk.app installation not found at $tex_location. Version: $tex_version"
+    tex_version=0
+fi
+
+# Check that an installation in Applications is also present
+if [[ -d "$standalone_location" ]]; then
+    standalone_version=$(get_version "$standalone_location")
+    echo "BibDesk.app installation found at $standalone_location. Version: $standalone_version"
+else
+    echo "BibDesk.app installation not found at $standalone_location. Version: $standalone_version"
+    standalone_version=0
+fi
+
+# Compare versions. Delete existing if version is older or equal to TeX installation
+if [[ $standalone_version == 0 ]]; then
+    echo "No standalone version. Nothing to do."
+elif is-at-least "$standalone_version" "$tex_version"; then
+    echo "TeX version ($tex_version) is newer or equal. Deleting standalone version ($standalone_version)."
+    if /bin/rm -rf "$standalone_location"; then
+        echo "Deleted $standalone_location"
+    else
+        echo "WARNING: failed to delete $standalone_location"
+    fi
+else
+    echo "Standalone ($standalone_version) is newer than the TeX version ($tex_version). Leaving it there."
+fi
+
