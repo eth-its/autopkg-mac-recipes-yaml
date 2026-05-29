3f6be8a97c47a5269d99df4da730dc6a957281ad
cglattfelder <christian.glattfelder@ethz.ch>
2026-05-29 16:05:03 +0200

    SupportApp: Update preinstall, and uninstaller script

--- Scripts_Installers/SupportAppCustomisation-preinstall.zsh
+++ Scripts_Installers/SupportAppCustomisation-preinstall.zsh
@@ -1,67 +1,189 @@
 #!/bin/zsh
 
-sudo bash -c 'cat > /Library/Management/ETHZ/SupportApp/status_update.zsh << "EOF"
+cat > /Library/Management/ETHZ/SupportApp/status_update.zsh << "EOF"
 #!/bin/zsh
 
 # Support App Extension - Status update (Runs every time the Support app is opened)
 
 # Support App preference plist
-preference_file_location="/Library/Preferences/nl.root3.support.plist"
+support_pref_file="/Library/Preferences/nl.root3.support.plist"
+managed_pref_file="/Library/Managed Preferences/nl.root3.support.plist"
+rows_template="/Library/Management/ETHZ/SupportApp/Supportapp_rows_template.plist"
 
-# Set Variables
-SupportGroup=$(defaults read /Library/Managed\ Preferences/nl.root3.support SupportGroup)
-serialNumber=$(system_profiler SPHardwareDataType | awk -F": " "/Serial Number/ {print \$2}")
+# Get admin and system defaults
+SupportGroup=$(defaults read ${managed_pref_file} SupportGroup)
+TicketLink=$(defaults read ${managed_pref_file} SecondRowLinkLeft)
+KBTitle=$(defaults read ${managed_pref_file} FirstRowSubtitleLeft)
+KBLink=$(defaults read ${managed_pref_file} FirstRowLinkLeft)
+serialNumber=$(ioreg -c "IOPlatformExpertDevice" | awk -F '"' '/IOPlatformSerialNumber/ {print $4}')
+selfservice=$(defaults read /Applications/ETH\ Self\ Service.app/Contents/Info.plist CFBundleIdentifier || echo com.jamf.selfserviceplus)
 
-# Set Values of Support App Content
-defaults write "${preference_file_location}" FooterText -string "**This Mac is managed by $SupportGroup**\\n---------------------------------\\nSerial Number: $serialNumber"
+plist_commands=()
+add_set_command() {
+    local key_path="$1"
+    local value="$2"
+    [[ -n "$value" ]] && plist_commands+=(-c "Set ${key_path} ${value}")
+}
+
+# make sure the Rows dict exists already, if not, import the template
+defaults read /Library/Preferences/nl.root3.support.plist Rows >/dev/null
+if [[ $? -gt 0 ]] ; then
+   /usr/libexec/PlistBuddy -c "Merge ${rows_template}" "$support_pref_file"
+fi
+
+# Populate items in the new Rows structure
+add_set_command ":Rows:2:Items:0:Subtitle" ${KBTitle}
+add_set_command ":Rows:2:Items:0:Action" ${KBLink}
+add_set_command ":Rows:3:Items:0:Action" ${TicketLink}
+add_set_command ":Rows:2:Items:1:Action" ${selfservice}
 
 if [ -d "/Applications/Privileges.app" ]; then
-    defaults write "${preference_file_location}" SecondRowTitleRight -string "Admin Rights"
-	defaults write "${preference_file_location}" SecondRowSymbolRight -string "lock.fill"
-	defaults write "${preference_file_location}" SecondRowSubtitleRight -string "Add or Remove"
+    add_set_command ":Rows:3:Items:2:Title" 'Admin Rights' 
+    add_set_command ":Rows:3:Items:2:Symbol" 'lock.fill'
+    add_set_command ":Rows:3:Items:2:Subtitle" 'Add or Remove' 
+    add_set_command ":Rows:3:Items:2:Action" 'corp.sap.privileges' 
 else
-    defaults write "${preference_file_location}" SecondRowTitleRight -string "Check-In"
-    defaults write "${preference_file_location}" SecondRowSubtitleRight -string "Check-In"
-	defaults write "${preference_file_location}" SecondRowSymbolRight -string "arrow.trianglehead.2.clockwise"
+    # fetch weather from wttr.in, at most once per hour
+    currweather=/Library/Management/ETHZ/SupportApp/currentweather.txt
+    if [ ! -f "$currweather" ] || [ $(( $(date +%s) - $(stat -f %m "$currweather" 2>/dev/null || echo 0) )) -gt 3600 ]; then
+    	curl -fsS --max-time 4 'https://wttr.in/?format=%c%20%t'>$currweather
+    fi
+    weather=$(cat $currweather)
+    add_set_command ":Rows:3:Items:2:Subtitle" 'Weather App'
+    add_set_command ":Rows:3:Items:2:Symbol" 'cloud.sun.fill'
+    add_set_command ":Rows:3:Items:2:Title" ${weather}
+    add_set_command ":Rows:3:Items:2:Action" 'com.apple.weather' 
 fi
+
+if (( ${#plist_commands[@]} > 0 )); then
+    /usr/libexec/PlistBuddy "${plist_commands[@]}" "$support_pref_file"
+fi
+
+defaults write "${support_pref_file}" FooterText -string "**This Mac is managed by $SupportGroup**\\n---------------------------------\\nSerial Number: $serialNumber"
 
 EOF
 
-cat > /Library/Management/ETHZ/SupportApp/SecondRowRight.zsh << "EOF"
-#!/bin/zsh
+cat > /Library/Management/ETHZ/SupportApp/Supportapp_rows_template.plist << "EOF"
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>Rows</key>
+	<array>
+		<dict>
+			<key>Items</key>
+			<array>
+				<dict>
+					<key>Type</key>
+					<string>ComputerName</string>
+				</dict>
+				<dict>
+					<key>Type</key>
+					<string>MacOSVersion</string>
+				</dict>
+			</array>
+		</dict>
+		<dict>
+			<key>Items</key>
+			<array>
+				<dict>
+					<key>Type</key>
+					<string>Uptime</string>
+				</dict>
+				<dict>
+					<key>Type</key>
+					<string>Storage</string>
+				</dict>
+			</array>
+		</dict>
+		<dict>
+			<key>Items</key>
+			<array>
+				<dict>
+					<key>ActionType</key>
+					<string>URL</string>
+					<key>Action</key>
+					<string>action</string>
+					<key>Symbol</key>
+					<string>questionmark.circle.fill</string>
+					<key>Title</key>
+					<string>Need Help</string>
+					<key>Subtitle</key>
+					<string>loading..</string>
+					<key>Type</key>
+					<string>Button</string>
 
-#########################################################
-# Support App config SecondRowRight					    #
-# created by Philippe Scholl                            #
-# version 1.1										    #
-# copyright by Mac Product Center                       #
-# Date: 25.10.2024                                      #
-#########################################################
-
-# Support App preference plist
-preference_file_location="/Library/Preferences/nl.root3.support.plist"
-
-
-if [ -d "/Applications/Privileges.app" ]; then
-    open -b corp.sap.privileges
-else
-	defaults write "${preference_file_location}" SecondRowSymbolRight -string "progress.indicator"
-    defaults write "${preference_file_location}" SecondRowTitleRight -string "Please wait..."
-    defaults write "${preference_file_location}" SecondRowSubtitleRight -string "Please wait..."
-    
-    jamf policy
-    
-    sleep 2
-    
-    defaults write "${preference_file_location}" SecondRowSymbolRight -string "arrow.trianglehead.2.clockwise"
-    defaults write "${preference_file_location}" SecondRowTitleRight -string "Check-In"
-    defaults write "${preference_file_location}" SecondRowSubtitleRight -string "Check-In"
-fi
+				</dict>
+				<dict>
+					<key>Action</key>
+					<string>action</string>
+					<key>ActionType</key>
+					<string>App</string>
+					<key>Subtitle</key>
+					<string>Open the Self Service kiosk</string>
+					<key>Symbol</key>
+					<string>cart.fill</string>
+					<key>Title</key>
+					<string>ETH Self Service</string>
+					<key>Type</key>
+					<string>Button</string>
+				</dict>
+			</array>
+		</dict>
+		<dict>
+			<key>Items</key>
+			<array>
+				<dict>
+					<key>ActionType</key>
+					<string>URL</string>
+					<key>Action</key>
+					<string>action</string>
+					<key>Symbol</key>
+					<string>ticket</string>
+					<key>Title</key>
+					<string>Open a ticket</string>
+					<key>Type</key>
+					<string>ButtonMedium</string>
+				</dict>
+				<dict>
+					<key>Action</key>
+					<string>com.cisco.secureclient.gui</string>
+					<key>ActionType</key>
+					<string>App</string>
+					<key>Symbol</key>
+					<string>point.3.connected.trianglepath.dotted</string>
+					<key>Title</key>
+					<string>Start VPN</string>
+					<key>Type</key>
+					<string>ButtonMedium</string>
+				</dict>
+				<dict>
+					<key>ActionType</key>
+					<string>App</string>
+					<key>Action</key>
+					<string>action</string>
+					<key>Type</key>
+					<string>ButtonMedium</string>
+					<key>Symbol</key>
+					<string>progress.indicator</string>
+					<key>Title</key>
+					<string>Loading..</string>
+					<key>Subtitle</key>
+					<string>Loading..</string>
+				</dict>
+			</array>
+		</dict>
+	</array>
+</dict>
+</plist>
 
 EOF
-'
+
 chmod 755 /Library/Management/ETHZ/SupportApp/status_update.zsh
-chmod 755 /Library/Management/ETHZ/SupportApp/SecondRowRight.zsh
+chown root:wheel /Library/Management/ETHZ/SupportApp/status_update.zsh
 
-# Enable PrivilegedHelperTool
-/Applications/Support.app/Contents/Resources/install_privileged_helper_tool.zsh
+# remove old script if present
+rm -f /Library/Management/ETHZ/SupportApp/SecondRowRight.zsh || true
+
+# update status
+/Library/Management/ETHZ/SupportApp/status_update.zsh
--- Scripts_Uninstallers/Support-uninstall.sh
+++ Scripts_Uninstallers/Support-uninstall.sh
@@ -28,7 +28,7 @@ function silent_app_quit() {
             if pgrep -f "/$check_app_name" ; then
                 (( n=n+1 ))
                 sleep 1
-				echo "Graceful close attempt # $n"
+                echo "Graceful close attempt # $n"
             else
                 echo "$app_name closed."
                 break
@@ -52,9 +52,9 @@ echo "Removing application: ${app_name}"
 
 # Add standard path if none provided
 if [[ ! $app_name == *"/"* ]]; then
-	app_to_trash="/Applications/$app_name.app"
+    app_to_trash="/Applications/$app_name.app"
 else
-	app_to_trash="$app_name.app"
+    app_to_trash="$app_name.app"
 fi
 
 echo "Application will be deleted: $app_to_trash"
@@ -71,27 +71,72 @@ fi
 # also check to see if an additional app was ever created due to BundleID mismatch
 if [[ -d "/Applications/${app_name}/${app_name}.app" ]]; then
     echo "Folder will be deleted: /Applications/${app_name}/"
-    /bin/rm -Rf "/Applications/${app_name}" ||:
+    /bin/rm -Rf "/Applications/${app_name}" || true
 else
     echo "Folder not found: /Applications/${app_name}/"
 fi
 if [[ -d "/Applications/${app_name}.localized/${app_name}.app" ]]; then
     echo "Folder will be deleted: /Applications/${app_name}.localized/"
-    /bin/rm -Rf "/Applications/${app_name}.localized" ||:
+    /bin/rm -Rf "/Applications/${app_name}.localized" || true
 else
     echo "Folder not found: /Applications/${app_name}.localized/"
 fi
 
 # Additional files to delete
 echo "Deleting other files"
-launchctl bootout system "/Library/LaunchDaemons/nl.root3.support.plist" &> /dev/null
-/bin/rm -f /Library/LaunchAgents/nl.root3.support.plist ||:
-/bin/rm -f /Library/LaunchDaemons/nl.root3.support.plist ||:
-/bin/rm -f /usr/local/bin/SupportHelper ||:
-/bin/rm -rf /Library/Management/ETHZ/SupportApp ||:
+
+# Get logged-in GUI user for LaunchAgent bootout
+loggedInUser=$(/usr/sbin/scutil <<< "show State:/Users/ConsoleUser" | /usr/bin/awk '/Name :/ && ! /loginwindow/ { print $3 }')
+loggedInUID=""
+if [[ -n "$loggedInUser" ]]; then
+    loggedInUID=$(/usr/bin/id -u "$loggedInUser" 2>/dev/null)
+fi
+
+# Bootout LaunchDaemons
+if [[ -f "/Library/LaunchDaemons/nl.root3.support.plist" ]]; then
+    launchctl bootout system "/Library/LaunchDaemons/nl.root3.support.plist" >/dev/null 2>&1 || true
+fi
+
+if [[ -f "/Library/LaunchDaemons/nl.root3.support.helper.plist" ]]; then
+    launchctl bootout system "/Library/LaunchDaemons/nl.root3.support.helper.plist" >/dev/null 2>&1 || true
+fi
+
+# Bootout LaunchAgent in logged-in user's GUI context
+if [[ -n "$loggedInUID" && -f "/Library/LaunchAgents/nl.root3.support.plist" ]]; then
+    launchctl bootout "gui/$loggedInUID" "/Library/LaunchAgents/nl.root3.support.plist" >/dev/null 2>&1 || true
+else
+    echo "No logged-in GUI user found or LaunchAgent plist missing"
+fi
+
+# Kill remaining helper / support related processes
+/usr/bin/pkill -f "nl.root3.support.helper" >/dev/null 2>&1 || true
+/usr/bin/pkill -f "nl.root3.support" >/dev/null 2>&1 || true
+/usr/bin/pkill -f "/Applications/Support.app" >/dev/null 2>&1 || true
+
+# Remove files only if they exist
+if [[ -f "/Library/LaunchAgents/nl.root3.support.plist" ]]; then
+    /bin/rm -f "/Library/LaunchAgents/nl.root3.support.plist" || true
+fi
+
+if [[ -f "/Library/LaunchDaemons/nl.root3.support.plist" ]]; then
+    /bin/rm -f "/Library/LaunchDaemons/nl.root3.support.plist" || true
+fi
+
+if [[ -f "/Library/LaunchDaemons/nl.root3.support.helper.plist" ]]; then
+    /bin/rm -f "/Library/LaunchDaemons/nl.root3.support.helper.plist" || true
+fi
+
+if [[ -f "/Library/PrivilegedHelperTools/nl.root3.support.helper" ]]; then
+    /bin/rm -f "/Library/PrivilegedHelperTools/nl.root3.support.helper" || true
+fi
+
+if [[ -d "/Library/Management/ETHZ/SupportApp" ]]; then
+    /bin/rm -rf "/Library/Management/ETHZ/SupportApp" || true
+fi
+
 
 
 # Forget packages (works for all versions)
 echo "Forgetting packages"
-/usr/sbin/pkgutil --forget nl.root3.support ||:
-/usr/sbin/pkgutil --forget nl.root3.support.helper ||:
+/usr/sbin/pkgutil --forget nl.root3.support || true
+/usr/sbin/pkgutil --forget nl.root3.support.helper || true
