5937639affcf2d551e4667d12b8cceac9e5d239e
cglattfelder <christian.glattfelder@ethz.ch>
2026-06-08 15:37:06 +0200

    SupportAppCustomisation-v2: Modify weather symbol,given jq is available.

--- Scripts_Installers/SupportAppCustomisation-preinstall.zsh
+++ Scripts_Installers/SupportAppCustomisation-preinstall.zsh
@@ -41,14 +41,99 @@ refresh_weather_cache() {
         cache_age=$(( $(/bin/date +%s) - $(/usr/bin/stat -f %m "$weather_cache" 2>/dev/null || /bin/echo 0) ))
     fi
 
+    if [[ -f /usr/bin/jq ]] ; then wttr_query='j2' ; else wttr_query='%c%20%t' ; fi 
+
     if [[ ! -s "$weather_cache" || "$cache_age" -gt 3600 ]]; then
         /bin/mkdir -p "$support_app_dir"
-        if /usr/bin/curl -fsS --max-time 4 "https://wttr.in/?format=%c%20%t" -o "$tmp_file" 2>/dev/null; then
+        if /usr/bin/curl -fsS --max-time 4 "https://wttr.in/?format=$wttr_query" -o "$tmp_file" 2>/dev/null; then
             /bin/mv "$tmp_file" "$weather_cache"
         else
             /bin/rm -f "$tmp_file"
         fi
     fi
+}
+
+translate_weather_icon() { 
+sf_symbol="cloud.sun.fill"
+weather_code=$1
+
+if [[ "$weather_code" == "113" ]]; then
+    sf_symbol="sun.max.fill"
+
+elif [[ "$weather_code" == "116" ]]; then
+    sf_symbol="cloud.sun.fill"
+
+elif [[ "$weather_code" == "119" ]]; then
+    sf_symbol="cloud.fill"
+
+elif [[ "$weather_code" == "122" ]]; then
+    sf_symbol="smoke.fill"
+
+elif [[ "$weather_code" == "143" || \
+        "$weather_code" == "248" || \
+        "$weather_code" == "260" ]]; then
+    sf_symbol="cloud.fog.fill"
+
+elif [[ "$weather_code" == "176" || \
+        "$weather_code" == "263" || \
+        "$weather_code" == "353" ]]; then
+    sf_symbol="cloud.sun.rain.fill"
+
+elif [[ "$weather_code" == "266" || \
+        "$weather_code" == "293" || \
+        "$weather_code" == "296" ]]; then
+    sf_symbol="cloud.drizzle.fill"
+
+elif [[ "$weather_code" == "299" || \
+        "$weather_code" == "302" || \
+        "$weather_code" == "305" || \
+        "$weather_code" == "308" || \
+        "$weather_code" == "356" || \
+        "$weather_code" == "359" ]]; then
+    sf_symbol="cloud.heavyrain.fill"
+
+elif [[ "$weather_code" == "179" || \
+        "$weather_code" == "182" || \
+        "$weather_code" == "185" || \
+        "$weather_code" == "281" || \
+        "$weather_code" == "284" || \
+        "$weather_code" == "311" || \
+        "$weather_code" == "314" || \
+        "$weather_code" == "317" || \
+        "$weather_code" == "350" || \
+        "$weather_code" == "362" || \
+        "$weather_code" == "365" || \
+        "$weather_code" == "374" || \
+        "$weather_code" == "377" ]]; then
+    sf_symbol="cloud.sleet.fill"
+
+elif [[ "$weather_code" == "227" || \
+        "$weather_code" == "320" || \
+        "$weather_code" == "323" || \
+        "$weather_code" == "326" || \
+        "$weather_code" == "368" ]]; then
+    sf_symbol="cloud.snow.fill"
+
+elif [[ "$weather_code" == "230" || \
+        "$weather_code" == "329" || \
+        "$weather_code" == "332" || \
+        "$weather_code" == "335" || \
+        "$weather_code" == "338" || \
+        "$weather_code" == "371" || \
+        "$weather_code" == "395" ]]; then
+    sf_symbol="snowflake"
+
+elif [[ "$weather_code" == "200" || \
+        "$weather_code" == "386" ]]; then
+    sf_symbol="cloud.sun.bolt.fill"
+
+elif [[ "$weather_code" == "389" ]]; then
+    sf_symbol="cloud.bolt.rain.fill"
+
+elif [[ "$weather_code" == "392" ]]; then
+    sf_symbol="cloud.bolt.fill"
+fi
+echo "$sf_symbol"
 }
 
 # Get admin and system defaults
@@ -89,10 +174,18 @@ else
     weather="Weather"
     [[ -s "$weather_cache" ]] && weather="$(<"$weather_cache")"
 
+    if [[ $wttr_query == 'j2' ]]; then 
+        weather_symbol=$(translate_weather_icon $(echo $weather|jq -r '.current_condition[0].weatherCode'))
+        weather_text="$(echo $weather|jq -r '.current_condition[0].FeelsLikeC')°C"
+    else
+        weather_symbol="cloud.sun.fill"
+        weather_text=$weather
+    fi
+    add_set_command ":Rows:3:Items:2:Symbol" "$weather_symbol"
+    add_set_command ":Rows:3:Items:2:Title" "$weather_text"
     add_set_command ":Rows:3:Items:2:Subtitle" "Weather App"
-    add_set_command ":Rows:3:Items:2:Symbol" "cloud.sun.fill"
-    add_set_command ":Rows:3:Items:2:Title" "$weather"
     add_set_command ":Rows:3:Items:2:Action" "com.apple.weather"
+
 fi
 
 if (( ${#plist_commands[@]} > 0 )); then
@@ -110,6 +203,8 @@ killall cfprefsd
 touch "$support_pref_file"
 killall cfprefsd
 fi
+
+
 
 EOF
 
