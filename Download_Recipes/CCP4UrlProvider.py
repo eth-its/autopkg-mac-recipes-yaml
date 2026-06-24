e0185e537328f9964b9b98cc842e1e66d0333e09
cglattfelder <christian.glattfelder@ethz.ch>
2026-06-24 13:15:16 +0200

    CCP4: move downloading into custom processor

--- Download_Recipes/CCP4UrlProvider.py
+++ Download_Recipes/CCP4UrlProvider.py
@@ -2,84 +2,78 @@
 # -*- coding: utf-8 -*-
 
 """
+Processor to check for pre-existing (and valid) download given a version number, and return should_continue=False if that is the case.
+Download the required installer in case it is not present yet.  
+
 Based on F5transkriptURLProvider.py by Tim Keller
 https://github.com/TK5-Tim/its-unibas-recipes/blob/LogitecSync/F5transkript/F5transkriptURLProvider.py
 
-The resulting link should be formatted similary: https://www.megasoftware.net/do_force_download/MEGAX_10.1.8_installer.pkg
+
 Downloaded artifacts are currently named like : ccp4-9.0.015-shelx-arpwarp-macosarm.tar.gz
 """
 
 from __future__ import absolute_import, print_function
-import re
-from autopkglib import URLGetter
-import sys
+from autopkglib.URLGetter import URLGetter
 import os
 
-try:
-    # import for Python 3
-    from html.parser import HTMLParser
-    import html
-except ImportError:
-    # import for Python 2
-    from HTMLParser import HTMLParser
-
-VERSION_URL = "https://www.megasoftware.net/history"
-BASE_URL = "https://www.megasoftware.net/do_force_download/MEGA_"
-REGEX = ".*MEGA\ \d{2}\ version\ (\d{2}\.\d\.\d)\ build .*"
-
-# __all__ == ["CCP4URLProvider"]
-
 class CCP4URLProvider(URLGetter):
-    """Provides a download URL for the latest version of CCP4"""
+    """Downloads the latest version of CCP4"""
 
     description = __doc__
     input_variables = {
-        "USER_AGENT": {"description": "User Agent used for downloading", "required":False, "default":"mozilla macintosh"},
-        "latest_version": {"description": "The latest version, as located on the download page", "required":True, "default":"0"}
+        "latest_version": {"description": "The latest version, as located on the download page", "required":True, "default":"0"},
+        "sessionid": {"description": "The session id for downloading", "required":True, "default":"0"}
     }
     output_variables = {
-        "url": {"description": "URL to latest version"},
         "version": {"description": "The latest version found"},
         "should_continue": {"description": "TRUEPREDICATE or FALSEPREDICATE depending on Cache directory contents"},
-        "CCP4_tgz_file_name": {"description": "filename of the tar.gz to be stored on disk"}
+        "pathname": {"description": "filename of the tar.gz to be stored on disk"}
     }
 
-    def main(self):
-        USER_AGENT=self.env["USER_AGENT"]
-        latest_version=self.env["latest_version"]
-        #if sys.version_info.major < 3:
-        #    html_source = self.download(VERSION_URL,headers={'User-Agent':USER_AGENT})
-        #else:
-        #    html_source = self.download(VERSION_URL,headers={'User-Agent':USER_AGENT}).decode("utf-8")
-        #escaped_url = re.search(REGEX, html_source).group(1)
-        #unescaped_url = html.unescape(escaped_url)
-        suffix = "-shelx-arpwarp-macosarm.tar.gz"
-        #return_url = BASE_URL + unescaped_url + suffix
-        self.env["version"] = latest_version
-        return_url='bogus'
-        self.env["url"] = return_url
+    def download_ccp4(sessionid,working_directory,CCP4_tgz_file_name):
+        downloadURL=f"https://www.ccp4.ac.uk/download/download_file.php?pkg=ccp4-shelx-arp-arm64&os=macos&sid={sessionid}"
 
-        # if we already have a pkg for the current version, we should not re-download
-        # autopkg requires Content-Length, ETAG or Last-Modified headers, but MEGA's web server sends none of these. 
-        # if a previous download were incomplete, it would be stored as another/temporary filename, so this should be safeish.
+        #first attempt - post that we accept the license for ccp4 ; this might suffice, and if we get a 3GB+ file, we can stop there.
+        self.env["curl_opts"] = ['-X','POST','-F',f'id={sessionid}','-F','package=ccp4','-F','result=accept','-F','accept=accept']
+        self.download_to_file(downloadURL,working_directory + "first_license_accepted.download")
+
+        if os.path.getsize(working_directory + "first_license_accepted.download") > 3000000000 :
+            os.rename(working_directory + "first_license_accepted.download",working_directory + CCP4_tgz_file_name)
+            return
+        
+        else:
+            os.remove(working_directory + "first_license_accepted.download")
+
+        #second attempt - post that we accept the license for shelx as well ; 
+        self.env["curl_opts"] = ['-X','POST','-F',f'id={sessionid}','-F','package=ccp4','-F','result=accept','-F','accept=accept']
+        self.download_to_file(downloadURL,working_directory + CCP4_tgz_file_name)
+
+    def main(self):
+        latest_version=self.env["latest_version"]
+        suffix = "-shelx-arpwarp-macosarm.tar.gz"
+        self.env["version"] = latest_version
+        sessionid = self.env["sessionid"]
 
         CCP4_tgz_file_name = "ccp4-" + latest_version + suffix  #ccp4-9.0.015-shelx-arpwarp-macosarm.tar.gz
         working_directory = self.env["RECIPE_CACHE_DIR"] + "/downloads/"
 
-        print(working_directory + CCP4_tgz_file_name)
+        print("CCP4URLPRovider: checking for presence of : " + working_directory + CCP4_tgz_file_name)
         if os.path.exists(working_directory + CCP4_tgz_file_name) :
-            self.env["should_continue"] = False
+            if os.path.getsize(working_directory + CCP4_tgz_file_name) > 3000000000 : 
+                self.env["should_continue"] = False
+            else:
+                os.remove(working_directory + CCP4_tgz_file_name)
+                download_ccp4(sessionid,working_directory,CCP4_tgz_file_name)
+                self.env["should_continue"] = True
         else:
+            download_ccp4(sessionid,working_directory,CCP4_tgz_file_name)
             self.env["should_continue"] = True
 
-        self.env["CCP4_tgz_file_name"] = CCP4_tgz_file_name
-
-        print(
-            "CCP4URLProvider: Suffix is: %s\n"
-            "CCP4URLProvider: Returning full url: %s "
-            % (suffix, return_url)
-        )
+        self.env["pathname"] = CCP4_tgz_file_name
 
 if __name__ == "__main__":
     processor = CCP4URLProvider()
     processor.execute_shell()
+
+
+        
