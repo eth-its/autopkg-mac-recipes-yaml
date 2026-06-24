a69abc5cb209187403bfb855dd2a4d591b81dec9
cglattfelder <christian.glattfelder@ethz.ch>
2026-06-24 13:46:42 +0200

    CCP4: Processor

--- Download_Recipes/CCP4UrlProvider.py
+++ Download_Recipes/CCP4UrlProvider.py
@@ -64,10 +64,10 @@ class CCP4URLProvider(URLGetter):
                 self.env["should_continue"] = False
             else:
                 os.remove(working_directory + CCP4_tgz_file_name)
-                self.download_ccp4(sessionid,working_directory,CCP4_tgz_file_name)
+                self.download_ccp4(sessionid=sessionid,working_directory=working_directory,CCP4_tgz_file_name=CCP4_tgz_file_name)
                 self.env["should_continue"] = True
         else:
-            self.download_ccp4(sessionid,working_directory,CCP4_tgz_file_name)
+            self.download_ccp4(sessionid=sessionid,working_directory=working_directory,CCP4_tgz_file_name=CCP4_tgz_file_name)
             self.env["should_continue"] = True
 
 
