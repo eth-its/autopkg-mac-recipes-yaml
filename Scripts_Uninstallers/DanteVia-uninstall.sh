820199d80da7b8031737f1eec341d8cc5668cbce
cglattfelder <christian.glattfelder@ethz.ch>
2026-09-07 15:26:59 +0200

    DanteVia: v0.1

---
+++ Scripts_Uninstallers/DanteVia-uninstall.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+pkill "Dante Via"
+
+DANTE_VIA_UNINSTALL_SCRIPT="/Library/Application Support/Audinate/DanteVia/Resources/DanteViaUninstall.command"
+
+if [[ ! -f "$DANTE_VIA_UNINSTALL_SCRIPT" ]]; then
+    echo -e "Dante Via uninstallation failed!\n\nUninstaller script not found at\n'$DANTE_VIA_UNINSTALL_SCRIPT'"
+    exit 1
+fi
+
+exec "$DANTE_VIA_UNINSTALL_SCRIPT"
